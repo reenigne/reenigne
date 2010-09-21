948b6f157f63864299caad0e695609fbea124d6e
Andrew Jenner <andrew@reenigne.org>
2010-09-20 22:02:25 -0700

    Simulator add/remove work

--- intervals/simulator/simulator.cpp
+++ intervals/simulator/simulator.cpp
@@ -2,6 +2,7 @@
 #include "unity/file.h"
 #include <vector>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <time.h>
 
@@ -100,11 +101,11 @@ private:
 
 class Simulation;
 
-class Bar : public ReferenceCounted
+template<class Simulation> class BarTemplate : public ReferenceCounted
 {
 public:
-    Bar(Simulation* simulation, const Program* program, bool root = false)
-      : _simulation(simulation), _program(program), _t(0), _root(root), _debug(false)
+    BarTemplate(Simulation* simulation, const Program* program, bool root = false)
+      : _simulation(simulation), _program(program), _t(0), _root(root), _debug(false), _readSubCycle(true)
     {
         for (int i = 0; i < 0x20; ++i)
             _memory[i] = 0;
@@ -124,29 +125,35 @@ public:
                 printf("        ");
             printf("Simulating to %lf\n", t);
         }
-        do {
-            if (_t > t)
-                return;
-            if (_debug) {
-                if (_root)
-                    printf("        ");
-                printf("%lf ", _t);
-            }
-            _t += 0.25*_tPerCycle;
-            simulateToRead();
-            if (_t > t)
-                return;
-            _t += 0.75*_tPerCycle;
-            simulateToWrite();
-        } while (true);
+        while (_t < t)
+            simulateSubcycle();
     }
     void adjustTime(double t) { _t -= t; }
-    void simulateToRead()
+    void simulateSubcycle()
     {
         if (_skipping) {
             _skipping = false;
+            _t = _t + 1.0;
             return;
         }
+        if (_debug) {
+            if (_root)
+                printf("        ");
+            printf("%lf ", _t);
+        }
+        if (_readSubCycle) {
+            _t += 0.25*_tPerCycle;
+            simulateToRead();
+            _readSubCycle = false;
+        }
+        else {
+            _t += 0.75*_tPerCycle;
+            simulateToWrite();
+            _readSubCycle = true;
+        }
+    }
+    void simulateToRead()
+    {
         int op = _program->op(_pch | _memory[2]);
         incrementPC();
         UInt16 r;
@@ -226,19 +233,19 @@ public:
                         break;
                     case 0x3:
                         if (_debug) printf("DECF 0x%02x, %c\n", _f, dc);
-                        storeZ(readMemory(f) - 1, d);
+                        storeZ(readMemory(_f) - 1, d);
                         break;
                     case 0x4:
                         if (_debug) printf("IORWF 0x%02x, %c\n", _f, dc);
-                        storeZ(readMemory(f) | _w, d);
+                        storeZ(readMemory(_f) | _w, d);
                         break;
                     case 0x5:
                         if (_debug) printf("ANDWF 0x%02x, %c\n", _f, dc);
-                        storeZ(readMemory(f) & _w, d);
+                        storeZ(readMemory(_f) & _w, d);
                         break;
                     case 0x6:
                         if (_debug) printf("XORWF 0x%02x, %c\n", _f, dc);
-                        storeZ(readMemory(f) ^ _w, d);
+                        storeZ(readMemory(_f) ^ _w, d);
                         break;
                     case 0x7:
                         if (_debug) printf("ADDWF 0x%02x, %c\n", _f, dc);
@@ -258,15 +265,15 @@ public:
                         break;
                     case 0x8:
                         if (_debug) printf("MOVF 0x%02x, %c\n", _f, dc);
-                        storeZ(readMemory(f), d);
+                        storeZ(readMemory(_f), d);
                         break;
                     case 0x9:
                         if (_debug) printf("COMF 0x%02x, %c\n", _f, dc);
-                        storeZ(~readMemory(f), d);
+                        storeZ(~readMemory(_f), d);
                         break;
                     case 0xa:
                         if (_debug) printf("INCF 0x%02x, %c\n", _f, dc);
-                        storeZ(readMemory(f) + 1, d);
+                        storeZ(readMemory(_f) + 1, d);
                         break;
                     case 0xb:
                         if (_debug) printf("DECFSZ 0x%02x, %c\n", _f, dc);
@@ -285,13 +292,13 @@ public:
                         break;
                     case 0xd:
                         if (_debug) printf("RLF 0x%02x, %c\n", _f, dc);
-                        r = (readMemory(f) << 1) | (_memory[3] & 1);
+                        r = (readMemory(_f) << 1) | (_memory[3] & 1);
                         setCarry((r & 0x100) != 0);
                         store(r, d);
                         break;
                     case 0xe:
                         if (_debug) printf("SWAPF 0x%02x, %c\n", _f, dc);
-                        r = readMemory(f);
+                        r = readMemory(_f);
                         store((r >> 4) | (r << 4), d);
                         break;
                     case 0xf:
@@ -310,23 +317,23 @@ public:
                 int m = 1 << b;
                 switch (op >> 8) {
                     case 4:
-                        if (_debug) printf("BCF 0x%02x, %i\n", f, b);
-                        writeMemory(f, readMemory(f) & ~m);
+                        if (_debug) printf("BCF 0x%02x, %i\n", _f, b);
+                        _data = readMemory(_f) & ~m;
                         break;
                     case 5:
-                        if (_debug) printf("BSF 0x%02x, %i\n", f, b);
-                        writeMemory(f, readMemory(f) | m);
+                        if (_debug) printf("BSF 0x%02x, %i\n", _f, b);
+                        _data = readMemory(_f) | m;
                         break;
                     case 6:
-                        if (_debug) printf("BTFSC 0x%02x, %i\n", f, b);
-                        if ((readMemory(f, m) & m) == 0) {
+                        if (_debug) printf("BTFSC 0x%02x, %i\n", _f, b);
+                        if ((readMemory(_f, m) & m) == 0) {
                             incrementPC();
                             _skipping = true;
                         }
                         break;
                     case 7:
-                        if (_debug) printf("BTFSS 0x%02x, %i\n", f, b);
-                        if ((readMemory(f, m) & m) != 0) {
+                        if (_debug) printf("BTFSS 0x%02x, %i\n", _f, b);
+                        if ((readMemory(_f, m) & m) != 0) {
                             incrementPC();
                             _skipping = true;
                         }
@@ -382,14 +389,12 @@ public:
     }
     void simulateToWrite()
     {
-        if (_skipping)
+        if (_f == -1)
             return;
-        if (_address == -1)
-            return;
-        if (_address == 0)
-            _address = _memory[4] & 0x1f;
-        _memory[_address] = _data;
-        if (_address == 6 || _address == 0x20) {
+        if (_f == 0)
+            _f = _memory[4] & 0x1f;
+        _memory[_f] = _data;
+        if (_f == 6 || _f == 0x20) {
             UInt8 h = _memory[6] | _memory[0x20];
             if ((h & 0x10) != (_io & 0x10))
                 _simulation->write(_t, _connectedBar[0], _connectedDirection[0], (h & 0x10) != 0);
@@ -406,7 +411,7 @@ public:
                 printf("Wrote 0x%02x\n", h);
             }
         }
-        if (address == 2)
+        if (_f == 2)
             _pch = 0;
     }
     void connect(int direction, int connectedBar, int connectedDirection)
@@ -414,45 +419,46 @@ public:
         _connectedBar[direction] = connectedBar;
         _connectedDirection[direction] = connectedDirection;
     }
-    bool read(double t, int direction) const
+    bool read(double t, int direction)
     {
         simulateTo(t);
-        switch (direction)
+        switch (direction) {
             case 0: return (_io & 0x10) != 0;
             case 1: return (_io & 0x20) != 0;
             case 2: return (_io & 1) != 0;
             case 3: return (_io & 2) != 0;
         }
+        return true;
     }
-    void prime()
+    void prime(int parent)
     {
         _primed = true;
-        int childB = _connectedBar[(parent + 1) & 3];
-        int childC = _connectedBar[(parent + 2) & 3];
-        int childD = _connectedBar[(parent + 3) & 3];
+        int childB = (parent + 1) & 3;
+        int childC = (parent + 2) & 3;
+        int childD = (parent + 3) & 3;
         _childBpresent = (childB != -1);
         if (_childBpresent) {
-            Bar* bar = _simulation->bar(childB);
+            Bar* bar = _simulation->bar(_connectedBar[childB]);
             if (bar->primed())
                 _childBpresent = false;
             else
-                bar->prime();
+                bar->prime(_connectedDirection[childB]);
         }
         _childCpresent = (childC != -1);
         if (_childCpresent) {
-            Bar* bar = _simulation->bar(childC);
+            Bar* bar = _simulation->bar(_connectedBar[childC]);
             if (bar->primed())
                 _childCpresent = false;
             else
-                bar->prime();
+                bar->prime(_connectedDirection[childC]);
         }
         _childDpresent = (childD != -1);
         if (_childDpresent) {
-            Bar* bar = _simulation->bar(childD);
+            Bar* bar = _simulation->bar(_connectedBar[childD]);
             if (bar->primed())
                 _childDpresent = false;
             else
-                bar->prime();
+                bar->prime(_connectedDirection[childD]);
         }
     }
     int* storeExpectedStream(int parent, int* store)
@@ -471,13 +477,16 @@ public:
             *(store++) = _childDpresent ? 1 : 0;
         }
         if (_childBpresent)
-            store = _simulation->bar(_connectedBar[childB])->storeExpectedStream(_connectedDirections[childB], store);
+            store = _simulation->bar(_connectedBar[childB])->storeExpectedStream(_connectedDirection[childB], store);
         if (_childCpresent)
-            store = _simulation->bar(_connectedBar[childC])->storeExpectedStream(_connectedDirections[childC], store);
+            store = _simulation->bar(_connectedBar[childC])->storeExpectedStream(_connectedDirection[childC], store);
         if (_childDpresent)
-            store = _simulation->bar(_connectedBar[childD])->storeExpectedStream(_connectedDirections[childD], store);
+            store = _simulation->bar(_connectedBar[childD])->storeExpectedStream(_connectedDirection[childD], store);
         _primed = false;
+        return store;
     }
+    int connectedBar(int direction) const { return _connectedBar[direction]; }
+    int connectedDirection(int direction) const { return _connectedDirection[direction]; }
 private:
     UInt8 readMemory(int address, UInt8 care = 0xff)
     {
@@ -547,6 +556,7 @@ private:
         if (_memory[2] == 0)
             _pch ^= 0x100;
     }
+    bool primed() const { return _primed; }
 
     Simulation* _simulation;
     const Program* _program;
@@ -569,14 +579,18 @@ private:
     bool _childBpresent;
     bool _childCpresent;
     bool _childDpresent;
+    bool _readSubCycle;
 };
+
+typedef BarTemplate<Simulation> Bar;
 
 class Simulation
 {
 public:
-    Simulation() { }
-    void simulate()
+    Simulation()
       : _totalBars(100), _stream(_totalBars*8), _expectedStream(_totalBars*8), _t(0), _badStreamOk(false)
+    { }
+    void simulate()
     {
         Program intervalProgram(String("../intervals.HEX"));
         intervalProgram.load();
@@ -584,12 +598,15 @@ public:
         Program rootProgram(String("../root.HEX"));
         rootProgram.load();
 
+        Bar* root;
         for (int i = 0; i <= _totalBars; ++i) {
             Reference<Bar> bar;
-            if (i == 0)
-                bar = new Bar(&rootProgram);
+            if (i == 0) {
+                bar = new Bar(this, &rootProgram);
+                root = bar;
+            }
             else
-                bar = new Bar(&intervalProgram, true);
+                bar = new Bar(this, &intervalProgram, true);
             _bars.push_back(bar);
             bar->connect(0, -1, 0);
             bar->connect(1, -1, 0);
@@ -597,20 +614,82 @@ public:
             bar->connect(3, -1, 0);
         }
         _streamPointer = &_stream[0];
+        _connectedPairs = 0;
         do {
-            root->simulateCycle();
+            root->simulateSubcycle();
             ++_t;
             if (_t == 256) {
                 _t = 0;
                 for (std::vector<Reference<Bar> >::iterator i = _bars.begin(); i != _bars.end(); ++i)
                     (*i)->adjustTime(256.0);
             }
-            if (random() % 1000 == 0) {
+            if (rand() % 1000 == 0) {
                 _badStreamOk = true;
-                int n = random() % (4*_totalBars + 1);
-                // Examine connector n
-                // If it's connected, disconnect it.
-                // If it's not connected, pick a random disconnected connector of the opposite gender and connect it.
+                int n = rand() % (4*_totalBars + 1);
+                int barNumber = (n - 1)/4 + 1;
+                int connectorNumber = (n - 1)%4;
+                if (n == 0) {
+                    barNumber = 0;
+                    connectorNumber = 2;
+                }
+                Bar* bar = _bars[barNumber];
+                int connectedBarNumber = bar->connectedBar(connectorNumber);
+                int connectedDirection = bar->connectedDirection(connectorNumber);
+                if (connectedBarNumber == -1 && _connectedPairs < _totalBars*2) {
+                    // This connector is not connected - connect it to a random disconnected connector of the opposite gender
+                    Bar* otherBar;
+                    if (connectorNumber == 0 || connectorNumber == 3) {
+                        // This is a male connector.
+                        n = rand() % (1 + 2*_totalBars - _connectedPairs);
+                        for (connectedBarNumber = 0; connectedBarNumber <= _totalBars; ++connectedBarNumber) {
+                            otherBar = _bars[connectedBarNumber];
+                            if (connectedBarNumber > 0 && otherBar->connectedBar(1) == -1) {
+                                if (n == 0) {
+                                    connectedDirection = 1;
+                                    break;
+                                }
+                                --n;
+                            }
+                            if (otherBar->connectedBar(2) == -1) {
+                                if (n == 0) {
+                                    connectedDirection = 2;
+                                    break;
+                                }
+                                --n;
+                            }
+                        }
+                    }
+                    else {
+                        // This is a female connector.
+                        n = rand() % (2*_totalBars - _connectedPairs);
+                        for (connectedBarNumber = 1; connectedBarNumber <= _totalBars; ++connectedBarNumber) {
+                            otherBar = _bars[connectedBarNumber];
+                            if (otherBar->connectedBar(0) == -1) {
+                                if (n == 0) {
+                                    connectedDirection = 0;
+                                    break;
+                                }
+                                --n;
+                            }
+                            if (otherBar->connectedBar(3) == -1) {
+                                if (n == 0) {
+                                    connectedDirection = 3;
+                                    break;
+                                }
+                                --n;
+                            }
+                        }
+                    }
+                    bar->connect(connectorNumber, connectedBarNumber, connectedDirection);
+                    otherBar->connect(connectedDirection, barNumber, connectorNumber);
+                }
+                else {
+                    // This connector is connected - disconnect it.
+                    Bar* connectedBar = _bars[connectedBarNumber];
+                    bar->connect(connectorNumber, -1, 0);
+                    connectedBar->connect(connectedDirection, -1, 0);
+                    --_connectedPairs;
+                }
             }
         } while (true);
     }
@@ -623,7 +702,7 @@ public:
         if (!_badStreamOk) {
             int* streamPointer = &_stream[0];
             int* expectedStreamPointer = &_expectedStream[0];
-            _bars[0]->prime();
+            _bars[0]->prime(0);
             int* expectedStreamPointerEnd = _bars[0]->storeExpectedStream(0, expectedStreamPointer);
             bool good = true;
             do {
@@ -643,7 +722,7 @@ public:
                 for (expectedStreamPointer = &_expectedStream[0]; expectedStreamPointer != expectedStreamPointerEnd; ++expectedStreamPointer)
                     printf("%i", *expectedStreamPointer);
                 printf(", observed ");
-                for streamPointer = &_stream[0]; streamPointer != _streamPointer; ++streamPointer)
+                for (streamPointer = &_stream[0]; streamPointer != _streamPointer; ++streamPointer)
                     printf("%i", *streamPointer);
                 printf("\n");
             }
@@ -666,9 +745,6 @@ public:
     Bar* bar(int n) { return _bars[n]; }
 private:
     std::vector<Reference<Bar> > _bars;
-    std::vector<Reference<ConnectedBars> > _connectedBars;
-    DisconnectedMaleConnector _disconnectedMaleConnector;
-    DisconnectedFemaleConnector _disconnectedFemaleConnector;
     int _totalBars;
     std::vector<int> _stream;
     std::vector<int> _expectedStream;
@@ -676,6 +752,7 @@ private:
     int _totalConnected;
     int _t;
     bool _badStreamOk;
+    int _connectedPairs;
 };
 
 int main()
