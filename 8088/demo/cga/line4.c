fe49b5484abe4f2a082330f6630e711c90107286
Andrew Jenner <andrew@reenigne.org>
2011-06-08 22:16:39 -0700

    CGA graphics

--- 8088/demo/cga/line4.c
+++ 8088/demo/cga/line4.c
@@ -1,21 +1,12 @@
 void line(UInt16 x0, UInt16 y0, UInt16 x1, UInt16 y1, UInt8 c)
 {
     ES = 0xb800;
-    AX = y1-y0;
-    if (AX < 0)
-        AX = -AX;
-    bp_4 = AX;
-    BX = x1-x0;
-    if (BX < 0)
-        BX = -BX;
-    bp_2 = BX;
-    if (BX < AX) {
-        AX <<= 1;
-        bp_6 = AX;
-        BX <<= 1;
-        bp_8 = BX;
-        DI = -bp_2;
-        CX = x0;
+    dy = abs(y1 - y0);
+    dx = abs(x1 - x0);
+    if (dx < dy) {
+        dy2 = dy << 1;
+        dx2 = dx << 1;
+        UInt16 e = -dx;
         if (y0 > y1) {
             DX = x0;
             x0 = x1;
@@ -24,228 +15,314 @@ void line(UInt16 x0, UInt16 y0, UInt16 x1, UInt16 y1, UInt8 c)
             y0 = y1;
             y1 = DX;
         }
-        BX = ((y0 & 1) << 13) + (y0 >> 1) * 80 + (x0 >> 2);
-        CL = (3 - (x0 & 3)) << 1;
-        DX = bp_4;
-        o2ef = bp_8;
-        o345 = bp_8;
-        o3ob = bp_6;
-        o361 = bp_6;
-        CH = c;
-        if (
-
-
-  MOV AX,[BP+8]
-  CMP AX,[BP+4]
-  JL o2c1
-  JMP o364
-o2c1:
-  JMP o3oe
-o2c3:
-  CMP W[BP+0e],1
-  JZ c1xor
-  ES: MOV AL,B[BX]
-  MOV AH,3
-  SHL AH,CL
-  NOT AH
-  AND AL,AH
-  MOV AH,CH
-  SHL AH,CL
-  ADD AL,AH
-  ES: MOV B[BX],AL
-  JMP c1finplot
-c1xor:
-  MOV AL,CH
-  SHL AL,CL
-  ES: XOR B[BX],AL
-c1finplot:
-  ADD BX,02000
-  CMP BX,04000
-  JL o2ef
-  SUB BX,03fb0
-o2ef:
-  DEC DX
-  ADD DI,06789
-  JLE o3oe
-  INC CL
-  INC CL
-  CMP CL,6
-  JLE o3ob
-  XOR CL,CL
-  DEC BX
-o3ob:
-  SUB DI,06789
-o3oe:
-  OR DX,DX
-  JGE o2c3
-  JMP o4af
-o319:
-  CMP W[BP+0e],1
-  JZ c2xor
-  ES: MOV AL,B[BX]
-  MOV AH,3
-  SHL AH,CL
-  NOT AH
-  AND AL,AH
-  MOV AH,CH
-  SHL AH,CL
-  ADD AL,AH
-  ES: MOV B[BX],AL
-  JMP c2finplot
-c2xor:
-  MOV AL,CH
-  SHL AL,CL
-  ES: XOR B[BX],AL
-c2finplot:
-  ADD BX,02000
-  CMP BX,04000
-  JL o345
-  SUB BX,03fb0
-o345:
-  DEC DX
-  ADD DI,06789
-  JLE o364
-  DEC CL
-  DEC CL
-  JGE o361
-  MOV CL,6
-  INC BX
-o361:
-  SUB DI,06789
-o364:
-  OR DX,DX
-  JGE o319
-  JMP o4af
-
+        initPosition(x0, y0);
+        UInt16 count = dy + 1;
+        if (x1 < x0) while (count-->0) { plot(); down(); e += dx2; if (e > 0) { left();  e -= dy2; } }
+        else         while (count-->0) { plot(); down(); e += dx2; if (e > 0) { right(); e -= dy2; } }
+    }
     else {
+        dx2 = dx << 1;
+        dy2 = dy << 1;
+        UInt16 e = -dy;
+        if (x0 > x1) {
+            DX = x0;
+            x0 = x1;
+            x1 = DX;
+            DX = y0;
+            y0 = y1;
+            y1 = DX;
+        }
+        initPosition(x0, y0);
+        UInt16 count = dx + 1;
+        if (y1 < y0) { while (count-->0) { plot(); right(); e += dy2; if (e > 0) { up();   e -= dx2; } }
+        else           while (count-->0) { plot(); right(); e += dy2; if (e > 0) { down(); e -= dx2; } }
+    }
+}
 
-o36f:
-  SHL BX,1
-  MOV [BP-6],BX
-  SHL AX,1
-  MOV [BP-8],AX
-  MOV DI,[BP-4]
-  NEG DI
-  MOV CX,[BP+4]
-  MOV AX,[BP+6]
-  CMP CX,[BP+8]
-  JLE o3ca
-  MOV DX,[BP+4]
-  MOV AX,[BP+8]
-  MOV [BP+4],AX
-  MOV [BP+8],DX
-  MOV DX,[BP+6]
-  MOV AX,[BP+0a]
-  MOV [BP+6],AX
-  MOV [BP+0a],DX
-o3ca:
-  MOV BX,[BP+6]
-  AND BX,1
-  MOV CL,0d
-  SHL BX,CL
-  MOV AX,[BP+6]
-  SAR AX,1
-  MOV DX,80
-  MUL DX
-  ADD BX,AX
-  MOV AX,[BP+4]
-  SAR AX,1
-  SAR AX,1
-  ADD BX,AX
-  MOV AX,[BP+4]
-  AND AL,3
-  MOV CL,3
-  SUB CL,AL
-  SHL CL,1
-  MOV DX,[BP-2]
-  MOV AX,[BP-8]
-  CS: MOV W[o439+3],AX
-  CS: MOV W[o48c+3],AX
-  MOV AX,[BP-6]
-  CS: MOV W[o44f+2],AX
-  CS: MOV W[o4a4+2],AX
-  MOV CH,[BP+0c]
-  MOV AX,[BP+0a]
-  CMP AX,[BP+6]
-  JL o4o7
-  JMP o4a7
-o4o7:
-  JMP o452
-o4o9:
-  CMP W[BP+0e],1
-  JZ d1xor
-  ES: MOV AL,B[BX]
-  MOV AH,3
-  SHL AH,CL
-  NOT AH
-  AND AL,AH
-  MOV AH,CH
-  SHL AH,CL
-  ADD AL,AH
-  ES: MOV B[BX],AL
-  JMP d1finplot
-d1xor:
-  MOV AL,CH
-  SHL AL,CL
-  ES: XOR B[BX],AL
-d1finplot:
-  DEC CL
-  DEC CL
-  JGE o439
-  MOV CL,6
-  INC BX
-o439:
-  DEC DX
-  ADD DI,06789
-  JLE o452
-  SUB BX,02000
-  JGE o44f
-  ADD BX,03fb0
-o44f:
-  SUB DI,06789
-o452:
-  OR DX,DX
-  JGE o4o9
-  JMP o4af
-o45c:
-  CMP W[BP+0e],1
-  JZ d2xor
-  ES: MOV AL,B[BX]
-  MOV AH,3
-  SHL AH,CL
-  NOT AH
-  AND AL,AH
-  MOV AH,CH
-  SHL AH,CL
-  ADD AL,AH
-  ES: MOV B[BX],AL
-  JMP d2finplot
-d2xor:
-  MOV AL,CH
-  SHL AL,CL
-  ES: XOR B[BX],AL
-d2finplot:
-  DEC CL
-  DEC CL
-  JGE o48c
-  MOV CL,6
-  INC BX
-o48c:
-  DEC DX
-  ADD DI,06789
-  JLE o4a7
-  ADD BX,02000
-  CMP BX,04000
-  JL o4a4
-  SUB BX,03fb0
-o4a4:
-  SUB DI,06789
-o4a7:
-  OR DX,DX
-  JGE o45c
-o4af:
-  POP DI
-  POP SI
-  MOV SP,BP
-  POP BP
-  RET
+void line(UInt16 x0, UInt16 y0, UInt16 x1, UInt16 y1, UInt8 c)
+{
+    ES = 0xb800;
+    initPosition(x0, y0);
+    UInt16 dx = x1 - x0;
+    UInt16 dy = y1 - y0;
+    if (dy >= 0) {
+        if (dx >= 0) {
+            if (dx < dy) {
+                UInt16 e = -dy;
+                UInt16 count = dy + 1;
+                dx <<= 1;
+                dy <<= 1;
+                while (count-->0) { plot(); down();  e += dx; if (e > 0) { right(); e -= dy; } }
+            }
+            else {
+                UInt16 e = -dx;
+                UInt16 count = dx + 1;
+                dx <<= 1;
+                dy <<= 1;
+                while (count-->0) { plot(); right(); e += dy; if (e > 0) { down();  e -= dx; } }
+            }
+        }
+        else {
+            dx = -dx;
+            if (dx < dy) {
+                UInt16 e = -dy;
+                UInt16 count = dy + 1;
+                dx <<= 1;
+                dy <<= 1;
+                while (count-->0) { plot(); down();  e += dx; if (e > 0) { left();  e -= dy; } }
+            }
+            else {
+                UInt16 e = -dx;
+                UInt16 count = dx + 1;
+                dx <<= 1;
+                dy <<= 1;
+                while (count-->0) { plot(); left();  e += dy; if (e > 0) { down();  e -= dx; } }
+            }
+        }
+    }
+    else {
+        dy = -dy;
+        if (dx >= 0) {
+            if (dx < dy) {
+                UInt16 e = -dy;
+                UInt16 count = dy + 1;
+                dx <<= 1;
+                dy <<= 1;
+                while (count-->0) { plot(); up();    e += dx; if (e > 0) { right(); e -= dy; } }
+            }
+            else {
+                UInt16 e = -dx;
+                UInt16 count = dx + 1;
+                dx <<= 1;
+                dy <<= 1;
+                while (count-->0) { plot(); right(); e += dy; if (e > 0) { up();    e -= dx; } }
+            }
+        }
+        else {
+            dx = -dx;
+            if (dx < dy) {
+                UInt16 e = -dy;
+                UInt16 count = dy + 1;
+                dx <<= 1;
+                dy <<= 1;
+                while (count-->0) { plot(); up();    e += dx; if (e > 0) { left();  e -= dy; } }
+            }
+            else {
+                UInt16 e = -dx;
+                UInt16 count = dx + 1;
+                dx <<= 1;
+                dy <<= 1;
+                while (count-->0) { plot(); left();  e += dy; if (e > 0) { up();    e -= dx; } }
+            }
+        }
+    }
+}
+
+// One way of specifying a line:
+//   x0 - 2 bytes
+//   y0 - 1 byte
+//   x1 - 2 bytes
+//   y1 - 1 byte
+//   colour - 1 byte
+//   total = 7 bytes
+// Another:
+//   routine - 1 byte
+//   major & number of pixels - 2 bytes
+//   minor & initia error - 2 bytes
+//   initial location - 2 bytes
+//   total = 7 bytes
+
+// Incoming:
+//   DL = colour
+//   DI = y0
+//   CX = x0
+//
+
+// Inner loop:
+//   AL = byte read or to write
+//   AH = mask
+//   DL = colour
+//   DH = temporary
+//   DI = screen memory location
+//   ES = screen memory segment
+//   CX = number of pixels to plot remaining
+//   SI = error
+//   BP = error increment
+//   SP = error decrement
+//   BL = inverse mask
+
+// TODO: unroll the loop by 2 for verticals, 4 for horizontals
+
+// Down major, right minor
+
+// Non-unrolled
+lineLoop:
+  es: xor [bx], ah     ; 3 2 20 23
+  add bh,020           ; 3 0 12  4
+  cmp bh,040           ; 3 0 12  4
+  jl oddLine           ; 2 0  8  4/16
+  sub bx,03fb0         ; 4 0 16  4
+oddLine:
+  add si,bp            ; 2 0  8  3
+  jle noAdjust         ; 2 0  8  4/16
+  shr ah,1             ; 2 0  8  2
+  shr ah,1             ; 2 0  8  2
+  jnc noNewByte        ; 2 0  8  4/16
+  mov ah,0c0           ; 2 0  8  4
+  inc bx               ; 1 0  4  2
+noNewByte:
+  sub si,di            ; 2 0  8  3
+noAdjust:
+  loop lineLoop        ; 2 0  8  5/17
+
+// Unrolled
+lineLoop0:
+  es: xor [bx], ah     ; 3 2 20 23
+  add bh,020           ; 3 0 12  4
+  add si,bp            ; 2 0  8  3
+  jle noAdjust0        ; 2 0  8  4/16
+  shr ah,1             ; 2 0  8  2
+  shr ah,1             ; 2 0  8  2
+  jnc noNewByte0       ; 2 0  8  4/16
+  mov ah,0c0           ; 2 0  8  4
+  inc bx               ; 1 0  4  2
+noNewByte0:
+  sub si,di            ; 2 0  8  3
+noAdjust0:
+  loop lineLoop1       ; 2 0  8  5/17
+  jmp done
+lineLoop1:
+  es: xor [bx], ah     ; 3 2 20 23
+  sub bx,01fb0         ; 4 0 16  4
+  add si,bp            ; 2 0  8  3
+  jle noAdjust1        ; 2 0  8  4/16
+  shr ah,1             ; 2 0  8  2
+  shr ah,1             ; 2 0  8  2
+  jnc noNewByte1       ; 2 0  8  4/16
+  mov ah,0c0           ; 2 0  8  4
+  inc bx               ; 1 0  4  2
+noNewByte1:
+  sub si,di            ; 2 0  8  3
+noAdjust1:
+  loop lineLoop0       ; 2 0  8  5/17
+done:
+
+// Right major, down minor
+
+// Non-unrolled
+lineLoop:
+  es: xor [bx], ah     ; 3 2 20 23
+  shr ah,1             ; 2 0  8  2
+  shr ah,1             ; 2 0  8  2
+  jnc noNewByte        ; 2 0  8  4/16
+  mov ah,0c0           ; 2 0  8  4
+  inc bx               ; 1 0  4  2
+noNewByte:
+  add si,bp            ; 2 0  8  3
+  jle noAdjust         ; 2 0  8  4/16
+  add bh,020           ; 3 0 12  4
+  cmp bh,040           ; 3 0 12  4
+  jl oddLine           ; 2 0  8  4/16
+  sub bx,03fb0         ; 4 0 16  4
+oddLine:
+  sub si,di            ; 2 0  8  3
+noAdjust:
+  loop lineLoop        ; 2 0  8  5/17
+
+// Unrolled
+lineLoop0:
+  es: xor [bx], al     ; 3 2 20 23
+  add si,bp            ; 2 0  8  3
+  jle noAdjust0        ; 2 0  8  4/16
+  add bh,020           ; 3 0 12  4
+  cmp bh,040           ; 3 0 12  4
+  jl oddLine0          ; 2 0  8  4/16
+  sub bx,03fb0         ; 4 0 16  4
+oddLine0:
+  sub si,sp            ; 2 0  8  3
+noAdjust0:
+  loop lineLoop1       ; 2 0  8  5/17
+  jmp done
+lineLoop1:
+  es: xor [bx], ah     ; 3 2 20 23
+  add si,bp            ; 2 0  8  3
+  jle noAdjust1        ; 2 0  8  4/16
+  add bh,020           ; 3 0 12  4
+  cmp bh,040           ; 3 0 12  4
+  jl oddLine1          ; 2 0  8  4/16
+  sub bx,03fb0         ; 4 0 16  4
+oddLine1:
+  sub si,di            ; 2 0  8  3
+noAdjust1:
+  loop lineLoop2       ; 2 0  8  5/17
+  jmp done
+lineLoop2:
+  es: xor [bx], dl     ; 3 2 20 23
+  add si,bp            ; 2 0  8  3
+  jle noAdjust2        ; 2 0  8  4/16
+  add bh,020           ; 3 0 12  4
+  cmp bh,040           ; 3 0 12  4
+  jl oddLine2          ; 2 0  8  4/16
+  sub bx,03fb0         ; 4 0 16  4
+oddLine2:
+  sub si,di            ; 2 0  8  3
+noAdjust2:
+  loop lineLoop3       ; 2 0  8  5/17
+  jmp done
+lineLoop3:
+  es: xor [bx], dh     ; 3 2 20 23
+  inc bx               ; 1 0  4  2
+  add si,bp            ; 2 0  8  3
+  jle noAdjust3        ; 2 0  8  4/16
+  add bh,020           ; 3 0 12  4
+  cmp bh,040           ; 3 0 12  4
+  jl oddLine3          ; 2 0  8  4/16
+  sub bx,03fb0         ; 4 0 16  4
+oddLine3:
+  sub si,di            ; 2 0  8  3
+noAdjust3:
+  loop lineLoop0       ; 2 0  8  5/17
+done:
+
+
+
+void draw_line(int xP, int yP, int xQ, int yQ)
+{
+    int x = xP;
+    int y = yP;
+    int D = 0;
+    int dx = xQ - xP;
+    int dy = yQ - yP;
+    int c;
+    int M;
+    int xinc = 1;
+    int yinc = 1;
+    if (dx < 0) { xinc = -1; dx = -dx; }
+    if (dy < 0) { yinc = -1; dy = -dy; }
+    if (dy < dx) {
+        c = 2 * dx;
+        M = 2 * dy;
+        while (x != xQ) {
+            putpix(x, y);
+            x += xinc;
+            D += M;
+            if (D > dx) {
+                y += yinc;
+                D -= c;
+            }
+        }
+    }
+    else {
+        c = 2 * dy;
+        M = 2 * dx;
+        while (y != yQ) {
+            putput(x, y);
+            y += yinc;
+            D += M;
+            if (D > dy) {
+                x += xinc
+                D -= c;
+            }
+        }
+    }
+}
