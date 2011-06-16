74540ef9bc215492743477483c9e382629ecc541
Andrew Jenner <andrew@reenigne.org>
2011-06-15 21:46:53 -0700

    5160 schematics

---
+++ 8088/demo/cga/raster/raster.cpp
@@ -0,0 +1,18 @@
+static const int nBars = 10;
+static const int barWidth = 7;
+int barColours[barWidth] = {4, 6, 14, 15, 11, 9, 1};
+
+int main()
+{
+    int barY[nBars];
+    FILE* out = fopen("raster.raw", "wb");
+    for (int t = 0; t < 320; ++t) {
+        for (int bar = 0; bar < nBars; ++bar)
+            barY[bar] =
+
+        for (int y = 0; y < 200; ++y) {
+
+        }
+    }
+    fclose(out);
+}
