bef88cb3a17b1ef4f9c64f38b9c98a9d1a087c71
Andrew Jenner <andrew@reenigne.org>
2014-02-03 22:02:14 +0000

    DMA work

--- 8088/8237.h
+++ 8088/8237.h
@@ -28,25 +28,52 @@ public:
     }
     void read()
     {
-        if(_address < 8)
-        {
-            this->set(_channels[_address >> 1].read(_address & 1));
+        if (_address < 8) {
+            this->set(_channels[_address >> 1].read(_address & 1, _lastByte));
+            _lastByte = !_lastByte;
+            return;
+        }
+        switch (address) {
+            case 8:
+                // TODO: read status register
+                // this->set(data);
+                return;
+            case 13:
+                // TODO: read temporary register
+                // this->set(data);
+                return;
         }
     }
     void write(UInt8 data)
     {
-        if(_address < 8)
-        {
-            _channels[_address >> 1].write(_address & 1,data);
+        if (_address < 8) {
+            _channels[_address >> 1].write(_address & 1, data, _lastByte);
+            _lastByte = !_lastByte;
+            return;
         }
-        switch(_address)
-        {
-            case 0x08: _command = data; break;
-            case 0x0B:
-            {
-                _channels[data & 3]._mode = data & 0xFC;
+        switch(_address) {
+            case 8: _command = data; break;
+            case 9: _channels[data & 3].setRequest((data & 4) != 0); break;
+            case 10: _channels[data & 3].setMask((data & 4) != 0); break;
+            case 11: _channels[data & 3].setMode(data & 0xfc); break;
+            case 12: _lastByte = false; break;
+            case 13:
+                // Master clear
+                _command = 0;
+                _status = 0;
+                _request = 0;
+                _temporary = 0;
+                _lastByte = false;
+                _mask = 15;
+                _state = stateIdle;
                 break;
-            }
+            case 14: _mask = 0; break;
+            case 15:
+                _channels[0].setMask((data & 1) ! = 0);
+                _channels[1].setMask((data & 2) ! = 0);
+                _channels[2].setMask((data & 4) ! = 0);
+                _channels[3].setMask((data & 8) ! = 0);
+                break;
         }
     }
     // Step 1: the device calls dmaRequest()
@@ -96,10 +123,9 @@ public:
 
     String save() const
     {
-        String s = String("{ ") + _dram.save() + ",\n  active: " +
-            String::Boolean(_active) + ", tick: " + _tick + ", address: " +
-            hex(_address, 5) + ", command: " + hex(_command, 2) +
-            ", channels: { ";
+        String s = String("{ active: " + String::Boolean(_active) +
+            ", tick: " + _tick + ", address: " + hex(_address, 5) +
+            ", command: " + hex(_command, 2) + ", channels: { ";
         bool needComma = false;
         for (int i = 0; i < 4; ++i) {
             if (needComma)
@@ -107,7 +133,7 @@ public:
             needComma = true;
             s += _channels[i].save();
         }
-        return s + " }\n";
+        return s + " }, lastByte: " + String::Boolean(_lastByte + " }\n";
     }
     Type type() const
     {
@@ -115,9 +141,10 @@ public:
         members.add(StructuredType::Member("active", false));
         members.add(StructuredType::Member("tick", 0));
         members.add(StructuredType::Member("address", 0));
-        members.add(StructuredType::Member("command", 4));
+        members.add(StructuredType::Member("command", 0));
         members.add(StructuredType::Member("channels",
             TypedValue(Type::array(_channels[0].type()), List<TypedValue>()));
+        members.add(StructuredType::Member("lastByte", false));
         return StructuredType("DMA", members);
     }
     void load(const TypedValue& value)
@@ -125,11 +152,12 @@ public:
         auto members = value.value<Value<HashTable<String, TypedValue>>>();
         _active = (*members)["active"].value<bool>();
         _tick = (*members["tick"].value<int>();
-        _address = (*members["address"].value<int>();
-        _command = (*members["command"].value<int>();
+        _address = (*members)["address"].value<int>();
+        _command = (*members)["command"].value<int>();
         auto channels = (*members)["channels"].value<List<TypedValue>>();
-        for (int i = 0; i < 4; ++i) {
+        for (int i = 0; i < 4; ++i)
             _channels[i].load((*i).value<TypedValue>());
+        _lastByte = (*members)["lastByte"].value<bool>();
     }
     String name() const { return "dma"; }
 private:
@@ -195,62 +223,55 @@ private:
                 }
             }
         }
-        UInt8 read(UInt32 address)
+        UInt8 read(UInt32 address, bool lastByte)
         {
-            switch(address) {
-                case 0:
-                    if (_firstbyte)
-                        return _startaddress & 0xff;
-                    return _startaddress >> 8;
-                case 1:
-                    if (_firstbyte)
-                        return _count & 0xff;
-                    return _count >> 8;
-            }
-            return 0;
+            int shift = (lastByte ? 8 : 0);
+            if (address == 0)
+                return (_currentAddress >> shift) & 0xff;
+            return (_currentCount >> shift) & 0xff;
         }
-        void write(UInt32 address, UInt8 data)
+        void write(UInt32 address, UInt8 data, bool lastByte)
         {
-            switch(address) {
-                case 0:
-                    if (_firstbyte)
-                        _startaddress = (_startaddress & 0xFF00) | data;
-                    else
-                        _startaddress = (_startaddress & 0xFF) | (data << 8);
-                    _transferaddress = _startaddress;
-                    break;
-                case 1:
-                    if (_firstbyte)
-                    {
-                        _count = (_count & 0xFF00) | data;
-                        _currentcount = _count;
-                    }
-                    else
-                    {
-                        _count = (_count & 0xFF) | (data << 8);
-                        _currentcount = _count;
-                    }
-                    break;
+            int shift = (lastByte ? 8 : 0);
+            UInt16 d = data << shift;
+            UInt16 m = 0xff << shift;
+            if (address == 0) {
+                _baseAddress = (_baseAddress & m) | d;
+                _currentAddress = (_currentAddress & m) | d;
+            }
+            else {
+                _baseCount = (_baseCount & m) | d;
+                _currentCount = (_currentCount & m) | d;
             }
         }
-        UInt8 _mode;
-        enum State
+        void setMode(UInt8 mode) { _mode = mode; }
+
+        String save() const
         {
-            stateIdle = 0,
-            stateS0,
-            stateS1,
-            stateS2,
-            stateS3,
-            stateS4
-        } _state;
-        UInt16 _transferaddress;
-        UInt16 _startaddress;
-        ISA8BitBus* _bus;
-        bool _dack;
+
+        }
+        Type type() const
+        {
+        }
+        void load(const TypedValue& value)
+        {
+        }
+
     private:
-        UInt16 _count;
-        UInt16 _currentcount;
-        bool _firstbyte;
+        UInt16 _baseAddress;
+        UInt16 _baseCount;
+        UInt16 _currentAddress;
+        UInt16 _currentCount;
+        UInt8 _mode;
+
+    //    UInt16 _transferaddress;     
+    //    UInt16 _startaddress;        
+    //    ISA8BitBus* _bus;
+    //    bool _dack;
+    //private:
+    //    UInt16 _count;
+    //    UInt16 _currentcount;
+    //    bool _firstbyte;
     };
 
     bool disabled() const { return (_command & 4) != 0; }
@@ -259,4 +280,16 @@ private:
     int _address;
     Byte _command;
     int _activechannel;
+    bool _lastByte;
+
+    enum State
+    {
+        stateIdle = 0,
+        stateS0,
+        stateS1,
+        stateS2,
+        stateS3,
+        stateS4
+    } _state;
+
 };
