aaee70ba36fc3770ebf8288110d2b091ba39b7f6
Andrew Jenner <andrew@reenigne.org>
2015-06-26 20:38:10 +0100

    3MHz VCR decoding

--- vcr_decode/vcr_decode.cpp
+++ vcr_decode/vcr_decode.cpp
@@ -9,7 +9,7 @@ float sinc(float z)
     return sin(z)/z;
 }
 
-static const int lobes = 5;
+static const int lobes = 35;
 
 float lanczos(float z)
 {
@@ -27,58 +27,46 @@ public:
         input.allocate(nn);
         inputHandle.read(&input[0], nn);
 
+        Array<Complex<float>> heterodyned(1820*2000);
+        Complex<float> localOscillator = 1;
+        Complex<float> localOscillatorFrequency = unit(3.9f*11/315);
+        for (int i = 0; i < 1820*2000; ++i) {
+            heterodyned[i] = static_cast<float>(input[i])*localOscillator;
+            localOscillator *= localOscillatorFrequency;
+        }
+
         Array<Byte> outputb(1820*2000);
-        int z = 2;
-        for (int x = 3*z; x < 1820*2000 - 3*z; ++x) {
-            // Sample rate is 28.6MHz
-            // Luma carrier is 3.4MHz to 4.4MHz = 6.5 to 8.4 samples
-            // d is tau/8.4 to tau/6.5 = 0.75 to 0.97
-            // c is 0.57 to 0.73
+        int z = 1;
+        float kernelSize = lobes;
+        float sampleFrequency = 157500000.0*2/11;
+        float cutoffFrequency = 3000000;
+        float cutoffSamples = sampleFrequency / (cutoffFrequency * 2); // 2.8
+        float lastPhase = 0;
 
-            // c = cos(d)
-            // s = sin(d)
-            // v = a + b*x + e*cos(d*x) + f*sin(d*x)
+        int firstInput = static_cast<int>(-kernelSize*cutoffSamples);
+        int lastInput = static_cast<int>(kernelSize*cutoffSamples);
+        for (int x = -firstInput; x < 1820*2000 - lastInput; ++x) {
+            Complex<float> v = 0;
+            float t = 0;
+            for (int j = firstInput; j <= lastInput; ++j) {
+                float s = lanczos(j/cutoffSamples);
+                Complex<float> z = s*heterodyned[j + x];
+                v += z;
+                t += s;
+            }
+            v /= t;
 
-            int v_3 = input[x-z*3];  // = a - 3*b + e*cos(3*d) - f*sin(3*d) 
-            int v_2 = input[x-z*2];  // = a - 2*b + e*cos(2*d) - f*sin(2*d)
-            int v_1 = input[x-z];    // = a - b + e*c - f*s
-            int v0 = input[x];       // = a + e
-            int v1 = input[x+z];     // = a + b + e*c + f*s
-            int v2 = input[x+z*2];   // = a + 2*b + e*cos(2*d) + f*sin(2*d)
-            int v3 = input[x+z*3];   // = a + 3*b + e*cos(3*d) + f*sin(3*d)
-
-            int o = v0;       // = a + e
-            int m = v_1 + v1; // = 2*a + 2*e*c
-            int n = v_2 + v2; // = 2*a + 2*e*cos(2*d) = 2*a + 2*e*(2*c*c - 1)
-            int p = v1 - v_1; // = 2*b + 2*f*s
-            int q = v2 - v_2; // = 4*b + 2*f*sin(2*d) = 4*b + 4*f*s*c
-            int r = v3 - v_3; // = 6*b + 2*f*sin(3*d) = 6*b + 2*f*(3*s + 4*s*s*s) = 6*b + 14*f*s - 8*c*c*f*s
-
-            int g0 = 2*m - 4*o; // = 4*e*(c - 1)
-            int h0 = 2*o - n;   // = 4*e*(1 - c*c)
-            int i0 = - h0 - g0; // = 4*e*(c*c - c)
-            int g1 = 2*q - 4*p; // = 8*f*s*(c - 1)
-            int h1 = r - 3*p;   // = 8*f*s*(1 - c*c)
-            int i1 = - h1 - g1; // = 8*f*s*(c*c - c)
-
-            // Now we have two quadratic equations in c: 
-            //   c*c*g0 + c*h0 + i0 = 0
-            //   c*c*g1 + c*h1 + i1 = 0
-
-            int k0 = h0*h0 - 4*g0*i0;
-            int k1 = h1*h1 - 4*g1*i1;
-            if (k0 < 0 || k1 < 0)
-                printf("Out of range!\n");
-            float j0 = sqrt(static_cast<float>(k0)); // = 4*e*(c*c - 2*c + 1)
-            float j1 = sqrt(static_cast<float>(k1)); // = 8*f*s*(c*c - 2*c + 1)
-
-            float c0 = (-h0 + j0)/(2*g0);  // The -j solutions give c = 1
-            float c1 = (-h1 + j1)/(2*g1);  
-
-            float c = c1; //(c0*g0*g0 + c1*g1*g1)/(g0*g0 + g1*g1);
-            float d = acos(c);
-            //outputb[x] = byteClamp((d-0.74)*255/(0.98 - 0.74));
-            outputb[x] = byteClamp(d*255/M_PI);
+            float phase = v.argument() / static_cast<float>(tau);
+            double deltaPhase = 1.5 + phase - lastPhase;
+            int deltaPhaseInt = static_cast<int>(deltaPhase);
+            deltaPhase -= deltaPhaseInt;
+            lastPhase = phase;
+            deltaPhase -= 0.5f;
+            deltaPhase *= 57.0f/2;
+            deltaPhase += 0.5f;
+            outputb[x] = byteClamp(255 - 255*deltaPhase);
+            //outputb[x] = static_cast<int>(255*phase);
+            //outputb[x] = static_cast<int>(heterodyned[x].x);
         }
         FileHandle h = File("D:\\t\\vcr_decoded.raw", true).openWrite();
         h.write(outputb);
