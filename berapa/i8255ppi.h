e2bcb7466e2fbc56039cc43a3fd73de42e488660
Andrew Jenner <andrew@reenigne.org>
2015-11-13 22:50:43 +0000

    PPI

--- berapa/i8255ppi.h
+++ berapa/i8255ppi.h
@@ -26,33 +26,46 @@ public:
     }
     void read()
     {
+        // IBF is reset by rising edge of RD input
+        if (_modeA == 1 && _directionA)
+            ibfA(false);
+        if (_modeB == 1 && _directionB)
+            ibfB(false);
+
+        // INTR is reset by falling edge of -RD
+        if (_modeA == 1 && _directionA)
+            intrA(false);
+        if (_modeB == 1 && _directionB)
+            intrB(false);
+
+
         UInt8 d = 0;
         switch(_address) {
             case 0:
                 if (_modeA == 0)
                     if (_directionA)
-                        d = _external[0];
+                        d = _incoming[0];
                     else
-                        d = _output[0];
+                        d = _outgoing[0];
                 break;
             case 1:
                 if (_modeB == 0)
                     if (_directionB)
-                        d = _external[1];
+                        d = _incoming[1];
                     else
-                        d = _output[1];
+                        d = _outgoing[1];
                 break;
             case 2:
                 if (_modeA == 0)
                     if (_directionCUpper)
-                        d = _external[2] & 0xf0;
+                        d = _incoming[2] & 0xf0;
                     else
-                        d = _output[2] & 0xf0;
+                        d = _outgoing[2] & 0xf0;
                 if (_modeB == 0)
                     if (_directionCLower)
-                        d |= _external[2] & 0x0f;
+                        d |= _incoming[2] & 0x0f;
                     else
-                        d |= _output[2] & 0xf0;
+                        d |= _outgoing[2] & 0x0f;
                 break;
             case 3:
                 // Disallowed by datasheet. TODO: figure out what this actually
@@ -70,34 +83,34 @@ public:
             case 0:
                 if (_modeA == 0)
                     if (!_directionA)
-                        set(0, data);
+                        outgoing(0, data);
                 break;
             case 1:
                 if (_modeB == 0)
                     if (!_directionB)
-                        set(1, data);
+                        outgoing(1, data);
                 break;
             case 2:
                 if (_modeA == 0)
                     if (_directionCUpper)
                         needSet = true;
                     else
-                        d = (d & 0x0f) | (_external[2] & 0xf0);
+                        d = (d & 0x0f) | (_outgoing[2] & 0xf0);
                 if (_modeB == 0)
                     if (_directionCLower)
                         needSet = true;
                     else
-                        d = (d & 0xf0) | (_external[2] & 0x0f);
+                        d = (d & 0xf0) | (_outgoing[2] & 0x0f);
                 if (needSet)
-                    set(2, data);
+                    outgoing(2, data);
                 break;
             case 3:
                 if ((data & 0x80) == 0) {
                     // Bit set/reset
                     if ((data & 1) == 0)
-                        _output[2] &= ~(1 << ((data >> 1) & 3));
+                        _portC &= ~(1 << ((data >> 1) & 3));
                     else
-                        _output[2] |= 1 << ((data >> 1) & 3);
+                        _portC |= 1 << ((data >> 1) & 3);
                 }
                 else {
                     // Mode set
@@ -106,15 +119,15 @@ public:
                     _directionB = ((data & 2) ! = 0);
                     _modeB = ((data & 4) >> 2);
 
-                    _directionCUppser = ((data & 8) != 0);
+                    _directionCUpper = ((data & 8) != 0);
                     _directionA = ((data & 0x10) != 0);
                     _modeA = ((data >> 5) & 3);
 
-                    set(0, 0);
-                    set(1, 0);
-                    set(2, 0);
+                    outgoing(0, _directionA ? 0xff : 0);
+                    outgoing(1, _directionB ? 0xff : 0);
+                    outgoing(2, (_directionCUpper ? 0xf0 : 0) |
+                        (_directionCLower ? 0x0f : 0));
                 }
-
         }
     }
     static String name() { return "Intel8255PPI"; }
@@ -154,12 +167,12 @@ public:
         return ISA8BitComponent::getValue(i);
     }
 
-    void setData(int i, Byte v) { _external[i] = v; }
+    void setData(int i, Byte v) { incoming(i, v); }
     void setData(int i, bool v)
     {
         int n = i >> 3;
         int b = 1 << (i & 7);
-        _external[n] = (_external[n] & ~b) | (v ? b : 0);
+        incoming(n, (_incoming[n] & ~b) | (v ? b : 0));
     }
 private:
     template<class T> class Connector : public BidirectionalConnector<T>
@@ -171,16 +184,81 @@ private:
         Intel8255PPI* _ppi;
         int _i;
     };
-    void set(int i, UInt8 v)
+    void outgoing(int i, UInt8 v)
     {
-        if (v != _output[i]) {  // TODO: is this right?
+        if (v != _outgoing[i]) {
             _bytes[i].other->setData(v);
             for (int b = 0; b < 8; ++b)
-                if (((v ^ _output[i]) & (1 << b)) != 0)
+                if (((v ^ _outgoing[i]) & (1 << b)) != 0)
                     _bits[(i<<3) | b].other->setData((v & (1 << b)) != 0);
-            _output[i] = v;
+            _outgoing[i] = v;
         }
     }
+    void checkInterrupts()
+    {
+        if (_modeA == 1 && _directionA && stbA() && ibfA() && inteAIn())
+            intrA(true);
+        if (_modeB == 1 && _directionB && stbB() && ibfB() && inteB())
+            intrB(true);
+    }
+    void incoming(int i, UInt8 v)
+    {
+        if (i == 2) {
+            if (_modeA == 1 && _directionA)
+                if (stbA() && (v & 0x10) == 0) {
+                    // -STB_A falling
+                    _input[0] = _incoming[0];
+                    ibfA(true);
+                }
+            if (_modeB == 1 && _directionB)
+                if ((_incoming[2] & 4) != 0 && (v & 4) == 0) {
+                    // -STB_B falling
+                    _input[1] = _incoming[1];
+                    ibfB(true);
+                }
+        }
+        _incoming[i] = v;
+        if (i == 2)
+            checkInterrupts();
+    }
+    void setOutgoing()
+    {
+        outgoing(0, _directionA ? 0xff : _port[0]);
+        outgoing(1, _directionB ? 0xff : _port[1]);
+        Byte d;
+        if (_modeB == 1)
+            d = 4 | ((_directionB ? _ibfB : _obfB) ? 2 : 0) | (_intrB ? 1 : 0);
+        else
+            d = ((_directionCLower ? 0xff : _port[2]) & 7);
+        if (_modeA != 0)
+            d |= (_intrA ? 8 : 0);
+        else
+            d |= ((_directionCLower ? 0xff : _port[2]) & 8);
+        if (_modeA == 2 && (_modeA == 1 && _directionA))
+            d |= (_ibfA ? 0x20 : 0) | 0x10;
+        else
+            d |= ((_directionCUpper ? 0xff : _port[2]) & 0x30);
+        if (_modeA == 2 && (_modeA == 1 && !_directionA))
+            d |= (_obfA ? 0x80 : 0) | 0x40;
+        else
+            d |= ((_directionCUpper ? 0xff : _port[2]) & 0xc0);
+        outgoing(2, d);
+    }
+
+    void obfA(bool x) { outgoing(2, (_outgoing[2] & ~0x80) | (x ? 0x80 : 0)); }
+    bool ackA() const { return (_incoming[2] & 0x40) != 0; }
+    void ibfA(bool x) { outgoing(2, (_outgoing[2] & ~0x20) | (x ? 0x20 : 0)); }
+    bool stbA() const { return (_incoming[2] & 0x10) != 0; }
+    bool ibfA() const { return (_outgoing[2] & 0x20) != 0; }
+    void intrA(bool x) { outgoing(2, (_outgoing[2] & ~8) | (x ? 8 : 0)); }
+    bool ackB() const { return (_incoming[2] & 4) != 0; }
+    void obfB(bool x) { outgoing(2, (_outgoing[2] & ~2) | (x ? 2 : 0)); }
+    void intrB(bool x) { outgoing(2, (_outgoing[2] & ~1) | (x ? 1 : 0)); }
+    bool stbB() const { return (_incoming[2] & 4) != 0; }
+    void ibfB(bool x) { outgoing(2, (_outgoing[2] & ~2) | (x ? 2 : 0)); }
+    bool inteAIn() const { return (_port[2] & 0x10) != 0; }
+    bool inteAOut() const { return (_port[2] & 0x40) != 0; }
+    bool inteB() const { return (_port[2] & 4) != 0; }
 
     Connector<Byte> _bytes[3];
     Connector<bool> _bits[24];
@@ -190,7 +268,23 @@ private:
     bool _directionB;
     bool _directionCUpper;
     bool _directionCLower;
-    Byte _external[3];
+    bool _obfA;
+    bool _ackA;
+    bool _ibfA;
+    bool _stbA;
+    bool _intrA;
+    bool _inteAIn;
+    bool _inteAOut;
+    bool _obfB;
+    bool _ackB;
+    bool _ibfB;
+    bool _stbB;
+    bool _intrB;
+
+    Byte _incoming[3];
+    Byte _outgoing[3];
+    Byte _input[2];
+    Byte _port[3];
     int _address;
     Intel8259PIC* _pic;
 };
