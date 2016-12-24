f47ea8e664662bc5700157b19246a0d748436b05
Andrew Jenner <andrew@reenigne.org>
2016-12-24 23:56:25 +0000

    More log compare

--- log_compare/log_compare.cpp
+++ log_compare/log_compare.cpp
@@ -1,9 +1,34 @@
 #include "alfe/main.h"
 #include "alfe/hash_table.h"
 
+enum TestState
+{
+    pass,
+    fail,
+    xpass,
+    kpass,
+    xfail,
+    kfail,
+    unresolved,
+    untested,
+    unsupported,
+    warning,
+    error
+};
+
 class TestResult
 {
+public:
+    TestState _state;
+    int _bytes;
+    int _cycles;
+};
 
+class TestResults
+{
+public:
+    TestResult _left;
+    TestResult _right;
 };
 
 class Program : public ProgramBase
@@ -17,6 +42,9 @@ class Program : public ProgramBase
         }
         String l1 = File(_arguments[1], true).contents();
         String l2 = File(_arguments[2], true).contents();
-        HashTable<String, TestResult> results;
+        HashTable<String, TestResults> results;
+        CharacterSource s1(l1);
+        CharacterSource s2(l2);
+
     }
 };
