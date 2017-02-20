ff5d41a2d56b8a3a468b0ca43ff414f65b6afb18
Andrew Jenner <andrew@reenigne.org>
2017-02-20 22:27:18 +0000

    More span buffer work

--- 8088/demo/cga/span/span.cpp
+++ 8088/demo/cga/span/span.cpp
@@ -5,6 +5,8 @@
 
 typedef Vector3<float> Point;
 
+DWORD cgaColours[4] = {0, 0x55ffff, 0xff55ff, 0xffffff};
+
 Point cubeCorners[8] = {
     Point(-1, -1, -1),
     Point(-1, -1,  1),
@@ -31,7 +33,7 @@ public:
     int _colour;
 };
 
-Quad cubeFaces[6][4] = {
+Quad cubeFaces[6] = {
     Quad(0, 4, 6, 2, 1),
     Quad(4, 5, 7, 6, 2),
     Quad(5, 1, 3, 7, 1),
@@ -40,11 +42,59 @@ Quad cubeFaces[6][4] = {
     Quad(0, 1, 5, 4, 3)
 };
 
+class Projection
+{
+public:
+    void init(float theta, float phi, float distance, Vector2<float> scale,
+        Vector2<float> offset)
+    {
+        float st = sin(theta);
+        float ct = cos(theta);
+        float sp = sin(phi);
+        float cp = cos(phi);
+        Vector2<float> s = scale*distance;
+        _xx = s.x*st;
+        _xy = -s.y*cp*ct;
+        _xz = -sp*ct;
+        _yx = s.x*ct;
+        _yy = s.y*cp*st;
+        _yz = sp*st;
+        _zy = s.y*sp;
+        _zz = -cp;
+        _distance = distance;
+        _offset = offset;
+    }
+    Point modelToScreen(Point model)
+    {
+        Point r(
+            _xx*model.x + _yx*model.y /*+ _zx*model.z*/,
+            _xy*model.x + _yy*model.y + _zy*model.z,
+            _xz*model.x + _yz*model.y + _zz*model.z + _distance);
+        return Point(r.x/r.z + _offset.x, r.y/r.z + _offset.y, r.z);
+    }
+private:
+    float _distance;
+    Vector2<float> _offset;
+    float _xx;
+    float _xy;
+    float _xz;
+    float _yx;
+    float _yy;
+    float _yz;
+    //float _zx;
+    float _zy;
+    float _zz;
+};
+
+
 class SpanWindow;
 
 class SpanBitmapWindow : public BitmapWindow
 {
 public:
+    SpanBitmapWindow() : _theta(0), _phi(0)
+    {
+    }
     void setSpanWindow(SpanWindow* window)
     {
         _spanWindow = window;
@@ -59,6 +109,25 @@ public:
             _bitmap = Bitmap<DWORD>(Vector(320, 200));
 
         _bitmap.fill(0);
+        Projection p;
+        _theta += 0.01;
+        if (_theta >= tau)
+            _theta -= tau;
+        _phi += 0.01*(sqrt(5.0) + 1)/2;
+        if (_phi >= tau)
+            _phi -= tau;
+        float ys = 99.5/sqrt(3.0);
+        float xs = 6*ys/5;
+        p.init(_theta, _phi, 5, Vector2<float>(xs, ys),
+            Vector2<float>(159.5, 99.5));
+
+        Point corners[8];
+        for (int i = 0; i < 8; ++i)
+            corners[i] = p.modelToScreen(cubeCorners[i]);
+
+        for (int i = 0; i < 6; ++i) {
+            Point p0 =
+        }
 
 
         _bitmap = setNextBitmap(_bitmap);
@@ -67,6 +136,8 @@ public:
 private:
     SpanWindow* _spanWindow;
     Bitmap<DWORD> _bitmap;
+    float _theta;
+    float _phi;
 };
 
 class SpanWindow : public RootWindow
