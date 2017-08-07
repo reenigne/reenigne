634198e897b21a91557f755e739ebf7968326e48
Andrew Jenner <andrew@reenigne.org>
2017-08-07 22:32:46 +0100

    Start of fade transition

---
+++ 8088/cga/transition/fade.asm
@@ -0,0 +1,187 @@
+org 0x100
+cpu 8086
+
+  mov ax,cs
+  mov ds,ax
+  cli
+  mov ss,ax
+  mov sp,stackHigh
+  sti
+
+  mov ax,0x40
+  mov ds,ax
+checkMotorShutoff:
+  cmp byte[0x40],0
+  je noMotorShutoff
+  mov byte[0x40],1
+  jmp checkMotorShutoff
+noMotorShutoff:
+
+  in al,0x21
+  mov [imr],al
+  mov al,0xfe  ; Enable IRQ0 (timer), disable all others
+  out 0x21,al
+
+  mov ax,3
+  int 0x10
+  mov dx,0x3d8
+  mov al,9
+  out dx,al
+  mov dl,0xd4
+  mov ax,0x0f03
+  out dx,ax
+  mov ax,0x7f04
+  out dx,ax
+  mov ax,0x6406
+  out dx,ax
+  mov ax,0x7007
+  out dx,ax
+  mov ax,0x0109
+  out dx,ax
+
+  mov al,0x34
+  out 0x43,al
+
+%macro setPIT0Count 1
+  mov al,(%1) & 0xff
+  out 0x40,al
+  %if ((%1) & 0xff) != ((%1) >> 8)
+  mov al,(%1) >> 8
+  %endif
+  out 0x40,al
+%endmacro
+
+  setPIT0Count 2  ; PIT was reset so we start counting down from 2 immediately
+
+%macro waitForVerticalSync 0
+  %%waitForVerticalSync:
+    in al,dx
+    test al,8
+    jz %%waitForVerticalSync       ;         jump if not +VSYNC, finish if +VSYNC
+%endmacro
+
+%macro waitForNoVerticalSync 0
+  %%waitForNoVerticalSync:
+    in al,dx
+    test al,8
+    jnz %%waitForNoVerticalSync    ;         jump if +VSYNC, finish if -VSYNC
+%endmacro
+
+  ; Wait for a while to be sure that IRQ0 is pending
+  waitForVerticalSync
+  waitForNoVerticalSync
+  waitForVerticalSync
+
+waitForDisplayEnable:
+  in al,dx
+  test al,1
+  jnz waitForDisplayEnable
+
+  setPIT0Count onScreenPitCycles
+
+  ; PIT channel 0 is now counting down from onScreenPitCycles in top half of onscreen area and IRQ0 is pending
+
+  mov ax,[0x20]
+  mov [cs:oldInterrupt8],ax
+  mov ax,[0x22]
+  mov [cs:oldInterrupt8+2],ax
+  mov word[0x20],transitionHandler
+  mov [0x22],cs
+
+  sti
+  jmp idle
+
+transitionHandler:
+  mov al,0x20
+  out 0x20,al
+
+  ; PIT channel 0 is now counting down from onScreenPitCycles in onscreen area
+
+  setPIT0Count offScreenPitCycles
+
+  ; When the next interrupt happens, PIT channel 0 will start counting down from offScreenPitCycles in offscreen area
+
+  mov word[0x20],offScreenHandler
+  mov [0x22],cs
+
+
+  sti
+
+idle:
+  hlt
+  jmp idle
+
+
+
+
+oldInterrupt8: dw 0, 0
+frameCount: dw 0, 0
+
+
+offScreenHandler:
+  mov al,0x20
+  out 0x20,al
+
+  xor ax,ax
+  mov ds,ax
+  mov word[0x20],onScreenHandler
+
+  setPIT0Count onScreenPitCycles
+
+  mov ax,cs
+  mov ss,ax
+  mov ds,ax
+  mov sp,updateBufferStart
+  mov ax,0xb800
+  mov es,ax
+  mov ds,[bufferSegment]
+  mov ch,0
+  pop si
+  pop di
+  pop bx
+  pop dx
+  ret
+
+offScreenHandlerEnd:
+  mov sp,stackHigh
+  sti
+  jmp idle
+
+
+onScreenHandler:
+  push cx
+  push bx
+  push di
+  push si
+  mov al,0x20
+  out 0x20,al
+
+  xor ax,ax
+  mov ds,ax
+  mov word[0x20],offScreenHandler
+
+  setPIT0Count offScreenPitCycles
+
+  lds si,[cs:soundPointer]
+  lodsw
+  out 0x42,al
+  mov al,ah
+  out 0x42,al
+  cmp si,[soundEnd]
+  jne noRestartSound
+  mov si,[soundStart]
+noRestartSound:
+  mov [soundPointer],si
+
+  inc word[frameCount]
+  jnz noFrameCountCarry
+  inc word[frameCount+2]
+noFrameCountCarry:
+
+
+section .bss
+
+stackLow:
+  resb 4096
+stackHigh:
+
