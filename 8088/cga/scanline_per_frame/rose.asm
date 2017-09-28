2581eaee01d644bbceab2d14cd28a357ba6195e0
Andrew Jenner <andrew@reenigne.org>
2017-09-28 22:30:18 +0100

    New rose work

--- 8088/cga/scanline_per_frame/rose.asm
+++ 8088/cga/scanline_per_frame/rose.asm
@@ -17,9 +17,9 @@ noMotorShutoff:
 
   in al,0x61
   or al,0x80
-  mov [port61high+1],al
+  mov [cs:port61high+1],al
   and al,0x7f
-  mov [port61low+1],al
+  mov [cs:port61low+1],al
 
   xor ax,ax
   mov ds,ax
@@ -29,11 +29,11 @@ noMotorShutoff:
   mov [cs:oldInterrupt8+2],ax
 
   in al,0x21
-  mov [imr],al
+  mov [cs:imr],al
   mov al,0xfe  ; Enable IRQ0 (timer), disable all others
   out 0x21,al
 
-  ; Determine and print phase
+  ; Determine phase
   lockstep 1
   mov ax,cs
   mov es,ax
@@ -104,239 +104,208 @@ initAddressesLoopTop:
   mov cx,200
 initRastersLoopTop:
   stosb
-;  inc ax
+  inc ax
   loop initRastersLoopTop
 
+  call copyImageData
 
+  jmp doneFrame
 
-  mov ax,0xb800
-  mov es,ax
-  mov ax,cs
-  mov ds,ax
-  mov si,data
-  xor di,di
-  mov cx,8000
-  cld
-  rep movsw
-  mov cx,192
-  xor ax,ax
-  rep stosw
-
-outputNumbers:
-  mov ax,0xb000
-  mov es,ax
-  call printNumbers
-
-  cmp word[numbersMode],0
-  je notNumbersMode
-
-  mov ax,1
-  int 0x10
-  mov ax,0xb800
-  mov es,ax
-  call printNumbers
-
-numbersScreen:
-  call doKeyboard
-  jmp numbersScreen
-
-notNumbersMode:
+restart:
 
   lockstep 1
 
-  xor ax,ax
-  mov ds,ax
-  mov word[0x20],dummyInterrupt8
-  mov [0x22],cs
-  mov ax,cs
-  mov ss,ax
-  mov sp,0xfffe
-
-  initCGA 9
-  mov dx,0x3d4
-  mov ax,0x0009
-  out dx,ax
-  mov ax,0x0005
-  out dx,ax
-
-  mov dl,0xda
-  waitForVerticalSync
-  waitForDisplayEnable
-  mov dl,0xd4
-  mov ax,0x0106
-  out dx,ax
-  mov ax,0x0104
-  out dx,ax
-  mov ax,0x0101
-  out dx,ax
-  mov ax,0x0100
-  out dx,ax
-
-
-
-  ; Mode                                                09
-  ;      1 +HRES                                         1
-  ;      2 +GRPH                                         0
-  ;      4 +BW                                           0
-  ;      8 +VIDEO ENABLE                                 8
-  ;   0x10 +1BPP                                         0
-  ;   0x20 +ENABLE BLINK                                 0
+  ; Mode
+  ;      1 +HRES
+  ;      2 +GRPH
+  ;      4 +BW
+  ;      8 +VIDEO ENABLE
+  ;   0x10 +1BPP
+  ;   0x20 +ENABLE BLINK
   mov dx,0x3d8
-  mov al,0x09
+  mov al,9
   out dx,al
 
-  ; Palette                                             00
-  ;      1 +OVERSCAN B                                   0
-  ;      2 +OVERSCAN G                                   2
-  ;      4 +OVERSCAN R                                   4
-  ;      8 +OVERSCAN I                                   0
-  ;   0x10 +BACKGROUND I                                 0
-  ;   0x20 +COLOR SEL                                    0
+  ; Palette
+  ;      1 +OVERSCAN B
+  ;      2 +OVERSCAN G
+  ;      4 +OVERSCAN R
+  ;      8 +OVERSCAN I
+  ;   0x10 +BACKGROUND I
+  ;   0x20 +COLOR SEL
   inc dx
-  mov al,15
+  mov al,0
   out dx,al
 
   mov dl,0xd4
-
-  ;   0xff Horizontal Total                             71
-  mov ax,0x0100
+  mov ax,0x7100
   out dx,ax
-
-  ;   0xff Horizontal Displayed                         50
-  mov ax,0x0101
+  mov ax,0x5001
   out dx,ax
-
-  ;   0xff Horizontal Sync Position                     5a
   mov ax,0x5a02
   out dx,ax
-
-  ;   0x0f Horizontal Sync Width                        0d
-  mov ax,0x0003 ;0x0f03
+  mov ax,0x0f03
   out dx,ax
-
-  ;   0x7f Vertical Total                               3d
-  mov ax,0x0104
+  mov ax,0x3f04
   out dx,ax
-
-  ;   0x1f Vertical Total Adjust                        00
   mov ax,0x0005
   out dx,ax
-
-  ;   0x7f Vertical Displayed                           02
   mov ax,0x0206
   out dx,ax
-
-  ;   0x7f Vertical Sync Position                       18
   mov ax,0x1907
   out dx,ax
-
-  ;   0x03 Interlace Mode                               02   0 = non interlaced, 1 = interlace sync, 3 = interlace sync and video
-  mov ax,0x0008 ; 0x0308
+  mov ax,0x0008
   out dx,ax
-
-  ;   0x1f Max Scan Line Address                        00
-  mov ax,0x0009
+  inc ax
   out dx,ax
-
-  ; Cursor Start                                        06
-  ;   0x1f Cursor Start                                  6
-  ;   0x60 Cursor Mode                                   0
   mov ax,0x060a
   out dx,ax
-
-  ;   0x1f Cursor End                                   07
-  mov ax,0x080b
+  mov ax,0x070b
   out dx,ax
-
-  ;   0x3f Start Address (H)                            00
   mov ax,0x000c
   out dx,ax
-
-  ;   0xff Start Address (L)                            00
-  mov ax,0x010d
+  inc ax
   out dx,ax
-
-  ;   0x3f Cursor (H)                                   03
   mov ax,0x3f0e
   out dx,ax
-
-  ;   0xff Cursor (L)                                   c0
   mov ax,0xff0f
   out dx,ax
+  mov dl,0xda
+  waitForNoVerticalSync
+  waitForVerticalSync
+  waitForDisplayEnable
+  mov ax,0x0104
+  mov dl,0xd4
+  out dx,ax
 
+  writePIT16 0, 2, 2   ; Ensure IRQ0 pending
 
   xor ax,ax
   mov ds,ax
-  mov word[0x20],interrupt8
-  mov word[0x22],cs
-  writePIT16 0, 2, 2         ; Ensure we have a pending IRQ0
+  mov word[0x20],interrupt8h0
+  mov [0x22],cs
 
-;  safeRefreshOff
-
-  mov dx,0x3da
+  mov dl,0xda
   waitForDisplayDisable
   waitForDisplayEnable
 
-  xor bx,bx
-  mov cx,60000
+  writePIT16 0, 2, 31
 
+  sti
+  hlt
+interrupt8h0:
+  mov al,75                 ; Now counting down from 31
+  out 0x40,al
+  mov al,0
+  out 0x40,al
+  mov word[0x20],interrupt8h1
+  mov al,0x20
+  out 0x20,al
+  sti
+  hlt
 
-  times 6 nop
-;  times 6 nop
-;  times 6 nop
-;  times 6 nop
+interrupt8h1:
+  in al,dx                  ; Now counting down from 75
+  test al,1
+  jz .noInterruptChange  ; jump if +DISPEN, finish if -DISPEN
+  mov word[0x20],interrupt8h2
+.noInterruptChange:
+  mov al,0x20
+  out 0x20,al
+  mov sp,stackTop
+  sti
+  hlt
 
+interrupt8h2:
+  mov ax,[cs:refreshPhase]     ; We're still counting down from 75
+  out 0x40,al
+  mov al,ah
+  out 0x40,al
+  mov word[0x20],interrupt8h3
+  mov al,0x20
+  out 0x20,al
+  mov sp,stackTop
+  sti
+  hlt
+
+interrupt8h3:
+  mov word[0x20],interrupt8h4  ; We're still counting down from refreshPhase
+  mov al,0x20
+  out 0x20,al
+  mov sp,stackTop
+  sti
+  hlt
+
+interrupt8h4:
+  refreshOn 19                 ; refreshPhase has happened, restart refresh
+  mov al,0x20
+  out 0x20,al
+  mov sp,stackTop
 
   mov dl,0xd4
-  mov ax,0x0101
-  out dx,ax
-  mov ax,0x2100
-  out dx,ax
-  mov ax,0x5a02
+  mov ax,0x3f04
   out dx,ax
 
+  mov dl,0xda
+  waitForNoVerticalSync
+  waitForVerticalSync
 
-  mov bx,[cs:initial]
-  add bx,timeSlide
-  call bx
+  waitForDisplayEnable
 
-  writePIT16 0, 2, 19912     ; Now counting down with the frame, one IRQ0 pending
+  writePIT16 0, 2, 76*64 - 1  ; Start counting down after display enable starts
 
-  ensureRefresh
-
-;  times 9 nop
-
-  mov al,TIMER1 | LSB | MODE2 | BINARY
-  out 0x43,al
-  mov al,19
-  out 0x41,al  ; Timer 1 rate
+  mov word[0x20],interrupt8a
 
   sti
   hlt
+interrupt8a:
+  in al,dx
+  test al,1
+  jz .noInterruptChange  ; jump if +DISPEN, finish if -DISPEN
+  mov word[0x20],interrupt8b
+.noInterruptChange:
+  mov al,0x20
+  out 0x20,al
+  mov sp,stackTop
+  sti
+  hlt
+
+interrupt8b:
+  mov ax,[cs:adjustPeriod]     ; We're still counting down from 76*64 - 1
+  out 0x40,al
+  mov al,ah
+  out 0x40,al
+  mov word[0x20],interrupt8c
+  mov al,0x20
+  out 0x20,al
+  mov sp,stackTop
+  sti
+  hlt
+
+interrupt8c:
+  mov ax,(76*262) & 0xff        ; We're still counting down from adjustPeriod
+  out 0x40,al
+  mov al,(76*262) >> 8
+  out 0x40,al
+  cmp byte[cs:stableImage],0
+  je notStableImage
+  mov word[0x20],interrupt8stable
+  jmp doneImageSelect
+notStableImage:
+  mov word[0x20],interrupt8
+doneImageSelect:
+  mov al,0x20
+  out 0x20,al
+  mov sp,stackTop
+  sti
+  hlt
+
 interrupt8:
-  mov al,0x20
-  out 0x20,al
-  xor ax,ax
-  mov ds,ax
-  mov word[0x20],interrupt8second
-  sti
-  hlt
-interrupt8second:
-  mov al,0x20
-  out 0x20,al
-
   mov ax,cs
   mov ds,ax
   mov ss,ax
   mov sp,startAddresses
   mov dx,0x3d4
-    pop cx
-    mov al,0x0c
-    mov ah,ch
-    out dx,ax
-    inc ax
-    mov ah,cl
-    out dx,ax
   mov bp,0x5001
   mov di,0x1900
   mov ax,0x5702
@@ -344,35 +313,34 @@ interrupt8second:
   mov bx,rasterData-sampleData
   mov es,ax
 
-  ; Scanlines 0-199
+  ; Scanlines -1..198
 
 %macro scanline 1
   mov al,0x00
   out dx,ax        ; e  Horizontal Total         left  0x5700  88
+
   mov ax,0x0202
   out dx,ax        ; f  Horizontal Sync Position right 0x0202   2
 
-  %if %1 != 199
-    pop cx
-    mov al,0x0c
-    mov ah,ch
-    out dx,ax
-    inc ax
-    mov ah,cl
-    out dx,ax
-  %else
-    mov ax,0x3f04
-    out dx,ax      ;    Vertical Total                 0x3f04  64  (2 for scanline 199, 62 for overscan)
-    times 9 nop  ; TODO: tune
-  %endif
+  pop cx
+  mov al,0x0c
+  mov ah,ch
+  out dx,ax
+  inc ax
+  mov ah,cl
+  out dx,ax
 
   lodsb
   out 0xe0,al
 
-  %if %1 == 0
+  %if %1 == -1
     mov ax,0x0104
     out dx,ax      ;    Vertical Total
-    times 2 nop
+    times 3 nop
+  %elif %1 == 198
+    mov ax,0x3f04
+    out dx,ax      ;    Vertical Total                 0x3f04  64  (1 for scanlines -1 and 198, 62 for scanlines 199-260)
+    times 3 nop
   %else
     mov al,[bx+si]
     mov dl,0xd9
@@ -391,13 +359,13 @@ interrupt8second:
   mov ax,es
   out dx,ax        ; c  Horizontal Sync Position left  0x5702  88
 %endmacro
-%assign i 0
+%assign i -1
 %rep 200
   scanline i
   %assign i i+1
 %endrep
 
-  ; Scanline 200
+  ; Scanline 199
 
   mov ax,0x7100
   out dx,ax        ; e  Horizontal Total         left  0x7100 114
@@ -408,12 +376,6 @@ interrupt8second:
   ; with no special timing requirements except:
   ;   HLT before overscan is over
   ;   Sound (if in use)
-
-;  mov ax,[frame]
-;  inc ax
-;  mov [frame],ax
-;  cmp ax,600
-;  je doneFrame
 
   mov ax,[increment]
   inc ax
@@ -438,15 +400,17 @@ interrupt8second:
   mov es,ax
   mov di,startAddresses
   mov bh,0
-;  mov si,lineTable
 %rep 200
   add dx,cx
   mov bl,dh
   mov si,bx
   mov ax,[bx+si+lineTable]
-;  mov ax,[bx+si]
   stosw
 %endrep
+
+endOfFrame:
+  mov al,0x20
+  out 0x20,al
 
   inc word[frameCount]
   jnz noFrameCountCarry
@@ -458,6 +422,18 @@ noFrameCountCarry:
   mov sp,stackTop
   sti
   hlt
+
+interrupt8stable:
+;  initCGA 9,0,2;0x0a
+  initCGA 0x0a
+  mov dl,0xd9
+  %rep 3800
+    out dx,al
+    inc ax
+  %endrep
+  mov ax,cs
+  mov ds,ax
+  jmp endOfFrame
 
 
 doKeyboard:
@@ -471,10 +447,100 @@ port61high:
 port61low:
   mov al,0x4f
   out 0x61,al
-  cmp bl,1
-  je tearDown
+  cmp bl,0x4b               ; left
+  je moveLeft
+  cmp bl,0x4d               ; right
+  je moveRight
+  cmp bl,0x48               ; up
+  je moveUp
+  cmp bl,0x50               ; down
+  je moveDown
+  cmp bl,0x4a               ; keypad-
+  je decreaseRefreshPhase
+  cmp bl,0x4e               ; keypad+
+  je increaseRefreshPhase
+  cmp bl,0x39               ; space
+  je switchCgaCrtcPhase
+  cmp bl,0x1f               ; s
+  je switchImage
+  cmp bl,0x31               ; n
+  je toggleNumbersScreen
+  cmp bl,1                  ; esc
+  je tearDown2
+  ret
+
+tearDown2:
+  jmp tearDown
+moveLeft:
+  dec word[adjustPeriod]
+  jmp doneFrame
+moveRight:
+  inc word[adjustPeriod]
+  jmp doneFrame
+moveUp:
+  sub word[adjustPeriod],76
+  jmp doneFrame
+moveDown:
+  add word[adjustPeriod],76
+  jmp doneFrame
+decreaseRefreshPhase:
+  dec word[refreshPhase]
+  cmp word[refreshPhase],64-1
+  jne doneFrame
+  mov word[refreshPhase],64+18
+  jmp doneFrame
+increaseRefreshPhase:
+  inc word[refreshPhase]
+  cmp word[refreshPhase],64+19
+  jne doneFrame
+  mov word[refreshPhase],64+0
+  jmp doneFrame
+switchCgaCrtcPhase:
+  xor byte[cgaCrtcPhase],1
+  jmp doneFrame
+switchImage:
+  xor byte[stableImage],1
+  cmp byte[numbersMode],0
+  jne .noCopyImageData
+  call copyImageData
+.noCopyImageData:
+  jmp doneFrame
+toggleNumbersScreen:
+  xor byte[numbersMode],1
+  cmp byte[numbersMode],0
+  je leavingNumbersMode
+
+  initCGA 9
+;  mov ax,3
+;  int 0x10
+;    mov ax,0xb800
+;    mov es,ax
+;    mov word[es:0],0x0745
+
+  jmp doneFrame
+leavingNumbersMode:
+  call copyImageData
+doneFrame:
+
+  mov sp,stackTop
+
+  mov ax,0xb000
+  call printNumbers
 
 
+  cmp byte[numbersMode],0
+  jne doNumbersMode
+  ; Not numbers mode, update numbers on MDA only
+
+  jmp restart
+doNumbersMode:
+
+  mov ax,0xb800
+  call printNumbers
+
+numbersScreen:
+  call doKeyboard
+  jmp numbersScreen
 
 
 tearDown:
@@ -536,13 +602,29 @@ exit:
   int 0x21
 
 
-dummyInterrupt8:
-  push ax
-  mov al,0x20
-  out 0x20,al
-  pop ax
-  iret
+copyImageData:
+  mov ax,0xb800
+  mov es,ax
+  mov ax,cs
+  mov ds,ax
+  xor di,di
+  cld
+  xor ax,ax
 
+  cmp byte[stableImage],0
+  jne clearVRAM
+
+  mov si,data
+  mov cx,8000
+  rep movsw
+  mov cx,192
+  rep stosw
+  ret
+
+clearVRAM:
+  mov cx,8192
+  rep stosw
+  ret
 
 printNybble:
   and al,0xf
@@ -578,11 +660,12 @@ printHex:
   ret
 
 printNumbers:
+  mov es,ax
   xor di,di
   mov ax,[phase]
   call printNybble
   mov di,160
-  mov ax,[initial]
+  mov ax,[adjustPeriod]
   call printHex
   mov di,320
   mov ax,[refreshPhase]
@@ -599,10 +682,11 @@ oldInterrupt8: dw 0, 0
 imr: db 0
 
 phase: dw 0
-initial: dw 3
-refreshPhase: dw 0
+adjustPeriod: dw 0x142c
+refreshPhase: dw 70
 cgaCrtcPhase: dw 0
 numbersMode: dw 0
+stableImage: dw 0
 
 
 lineTable:
