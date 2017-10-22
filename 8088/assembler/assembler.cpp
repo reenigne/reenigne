2cc65db5236b2eaa76bac6a154ae474c5e3561b8
Andrew Jenner <andrew@reenigne.org>
2017-10-22 22:43:21 +0100

    More assembler work

--- 8088/assembler/assembler.cpp
+++ 8088/assembler/assembler.cpp
@@ -18,3 +18,17 @@ void mov(Operand destination, Operand source)
 {
     currentInstructionChain.add(MovInstruction(destination, source));
 }
+
+void out(Operand port, Operand value)
+{
+    if (port >= 0x100)
+        mov(dx, port);
+    if (value.size() == 1) {
+        mov(al, value);
+        if (dx.value() == port
+    }
+    else
+        mov(ax, value);
+
+
+}
