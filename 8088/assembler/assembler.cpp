dd349442c0bcc782293a44c93720bffd058f2ef1
Andrew Jenner <andrew@reenigne.org>
2017-11-01 22:48:25 +0000

    More assembler work

--- 8088/assembler/assembler.cpp
+++ 8088/assembler/assembler.cpp
@@ -39,8 +39,8 @@ public:
     {
         file->addRegister(this);
     }
-    Word value() { return _value; }
-    bool isConstant() { return _symbol == zero; }
+    Word value() const { return _value; }
+    bool isConstant() const { return _symbol == zero; }
     virtual void assign(T value, Symbol symbol)
     {
         _value = value;
@@ -48,14 +48,15 @@ public:
         for (auto& i : _partOf)
             i->partAssigned(value, symbol, this);
     }
-    bool hasValue(Word v) { return isConstant() && value() == v; }
+    bool hasValue(Word v) const { return isConstant() && value() == v; }
     void clear() { assign(0, unknown); }
     void makePartOf(Register* larger)
     {
         _partOf.append(larger);
     }
     virtual void partAssigned(Word value, Symbol symbol, Register* part) { }
-    int width() { return _width; }
+    int width() const { return _width; }
+    int binaryEncoding() const { return _binaryEncoding; }
 protected:
     RegisterFile* _file;
     String _name;
@@ -112,7 +113,6 @@ public:
 private:
     Register<T2>* _lowPart;
     Register<T2>* _highPart;
-    int _binaryEncoding;
 };
 
 RegisterFile registerFile;
@@ -249,40 +249,240 @@ public:
     void assemble(Byte* p) { *p = 0x99; }
 };
 
-class Operand
+class RegisterExpression
 {
 public:
-    virtual bool wordSize() const = 0;
-};
-
-class RegisterOperand : public Operand
-{
-public:
-    RegisterOperand(Register* r) : _register(r) { }
-    bool wordSize() const { return _register->width() == 2; }
+    RegisterExpression(const Register* r, Word offset)
+      : _base(0), _index(0), _offset(offset)
+    {
+        addRegister(r);
+    }
+    RegisterExpression(const RegisterExpression& re, Word offset)
+    {
+        *this = re;
+        _offset += offset;
+    }
+    RegisterExpression(const RegisterExpression& re, Register* r)
+    {
+        *this = re;
+        addRegister(r);
+    }
+    RegisterExpression(const RegisterExpression& re1, RegisterExpression& re2)
+    {
+        *this = re1;
+        if (re2._base != 0)
+            addRegister(re2._base);
+        if (re2._index != 0)
+            addRegister(re2._index);
+        _offset += re2._offset;
+    }
+    int length() const
+    {
+        if (_offset == 0 && (_base != &bp || _index != 0))
+            return 1;
+        if (_offset >= -128 && _offset < 128 && (_base != 0 || _index != 0))
+            return 2;
+        return 3;
+    }
+    void assemble(int reg, Byte* p) const
+    {
+        Byte modrm;
+        if (_base == 0) {
+            if (_index == 0)
+                modrm = 6;
+            else {
+                if (_index == &si)
+                    modrm = 4;
+                else
+                    modrm = 5;
+            }
+        }
+        else {
+            if (_base == &bx) {
+                if (_index == 0)
+                    modrm = 7;
+                else {
+                    if (_index == &si)
+                        modrm = 0;
+                    else
+                        modrm = 1;
+                }
+            }
+            else {
+                if (_index == 0)
+                    modrm = 6;
+                else {
+                    if (_index == &si)
+                        modrm = 2;
+                    else
+                        modrm = 3;
+                }
+            }
+        }
+        *p = modrm + (reg << 3);
+        int l = length();
+        if (l > 1) {
+            p[1] = offset;
+            if (l > 2)
+                p[2] = offset >> 8;
+        }
+    }
+    bool defaultSS() const { return _base == &bp; }
 private:
-    Register* _register;
-};
-
-class MemoryOperand : public Operand
-{
-public:
-    bool wordSize() const { return _wordSize; }
-private:
-    bool _wordSize;
-    int _segment;
+    void addRegister(Register* r)
+    {
+        if (r == &bx || r == &bp) {
+            if (_base != 0)
+                throw Exception("Too many base registers in expression.");
+            _base = r;
+            return;
+        }
+        if (r == &si || r == &di) {
+            if (_index != 0)
+                throw Exception("Too many base registers in expression.");
+            _index = r;
+            return;
+        }
+        throw Exception("Bad register in expression");
+    }
     Word _offset;
-    Register* _base;
-    Register* _index;
+    const Register* _base;
+    const Register* _index;
 };
+
+RegisterExpression operator+(const Register& r, int offset)
+{
+    return RegisterExpression(&r, offset);
+}
+
+RegisterExpression operator+(int offset, const Register& r)
+{
+    return RegisterExpression(&r, offset);
+}
+
+RegisterExpression operator-(const Register& r, int offset)
+{
+    return RegisterExpression(&r, -offset);
+}
+
+RegisterExpression operator+(const RegisterExpression& re, int offset)
+{
+    return RegisterExpression(re, offset);
+}
+
+RegisterExpression operator+(int offset, const RegisterExpression& re)
+{
+    return RegisterExpression(re, offset);
+}
+
+RegisterExpression operator-(const RegisterExpression& re, int offset)
+{
+    return RegisterExpression(re, -offset);
+}
+
+RegisterExpression operator+(const Register& r, const RegisterExpression& re)
+{
+    return RegisterExpression(re, &r);
+}
+
+RegisterExpression operator+(const RegisterExpression& re, const Register& r)
+{
+    return RegisterExpression(re, &r);
+}
+
+RegisterExpression operator+(const RegisterExpression& re1,
+    const RegisterExpression& re2)
+{
+    return RegisterExpression(re1, re2);
+}
+
+class Operand : public Handle
+{
+public:
+    Operand(Register* r) : Handle(create<RegisterBody>(r)) { }
+    Operand(Register& r) : Handle(create<RegisterBody>(&r)) { }
+    Operand(bool wordSize, int segment, Word offset, const Register* base,
+        const Register* index)
+      : Handle(create<MemoryBody>(wordSize, segment, offset, base, index)
+    { }
+    bool wordSize() const { return body()->wordSize(); }
+    int length() const { return body()->length(); }
+    void assemble(int reg, Byte* p) const { body()->assemble(reg, p); }
+protected:
+    Operand(Handle h) : Handle(h) { }
+    class Body : public Handle::Body
+    {
+    public:
+        virtual bool wordSize() const = 0;
+        virtual int length() const { return 1; }
+        virtual void assemble(int reg, Byte* p) const = 0;
+    };
+    class RegisterBody : public Body
+    {
+    public:
+        RegisterBody(Register* r) : _register(r) { }
+        bool wordSize() const { return _register->width() == 2; }
+        void assemble(int reg, Byte* p) const
+        {
+            *p = _register->binaryEncoding() + 0xc0 + (reg << 3);
+        }
+    private:
+        Register* _register;
+    };
+    class MemoryBody : public Body
+    {
+    public:
+        MemoryBody(bool wordSize, int segment, RegisterExpression e)
+          : _wordSize(wordSize), _segment(segment), _e(e) { }
+        bool wordSize() const { return _wordSize; }
+        int length() const { return _e.length(); }
+        void assemble(int reg, Byte* p) const { _e.assemble(reg, p); }
+        int segment() { return _segment; }
+    private:
+        bool _wordSize;
+        int _segment;
+        RegisterExpression _e;
+    };
+    const Body* body() const { return as<Body>(); }
+};
+
+Operand word(Word offset, int segment = -1)
+{
+    return Operand(true, segment, RegisterExpression(offset, 0, 0));
+}
+
+Operand byte(Word offset, int segment = -1)
+{
+    return Operand(false, segment, RegisterExpression(offset, 0, 0));
+}
+
+Operand word(const Register& r, int segment = -1)
+{
+    return Operand(true, segment, RegisterExpression(0, &r, 0));
+}
+
+Operand byte(const Register& r, int segment = -1)
+{
+    return Operand(false, segment, RegisterExpression(0, &r, 0));
+}
+
+Operand word(const RegisterExpression& r, int segment = -1)
+{
+    return Operand(true, segment, r);
+}
+
+Operand byte(const RegisterExpression& r, int segment = -1)
+{
+    return Operand(false, segment, r);
+}
 
 class INCInstruction : public Instruction
 {
 public:
-    INCInstruction(
+    INCInstruction(const Operand& o) : _operand(o) { }
     void assemble(Byte* p) { *p = 0x99; }
 private:
-    std::unique_ptr<Operand> _operand;
+    Operand _operand;
 };
 
 class MOVInstruction : public Instruction
