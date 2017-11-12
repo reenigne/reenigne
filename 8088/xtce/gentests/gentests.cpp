d4bbe1d6c1c0e2df188d645147d314a33cbbdf33
Andrew Jenner <andrew@reenigne.org>
2017-11-12 08:55:30 +0000

    XTCE test generation work

--- 8088/xtce/gentests/gentests.cpp
+++ 8088/xtce/gentests/gentests.cpp
@@ -1,7 +1,211 @@
 #include "alfe/main.h"
 
+class Instruction
+{
+public:
+    Instruction() { }
+    Instruction(Byte opcode, Byte modrm = 0, Word offset = 0,
+        Word immediate = 0)
+      : _opcode(opcode), _modrm(modrm), _offset(offset), _immediate(immediate)
+    {
+    }
+    bool hasModrm()
+    {
+        static const Byte hasModrmTable[] = {
+            1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+            1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+            1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+            1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+            1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1};
+        return hasModrmTable[_opcode] != 0;
+    }
+    int immediateBytes()
+    {
+        static const Byte immediateBytesTable[] = {
+            0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0,
+            0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0,
+            0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0,
+            0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0,
+            2, 2, 2, 2, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0,
+            1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 2,
+            2, 0, 2, 0, 1, 1, 0, 0, 2, 0, 2, 0, 0, 1, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 4, 1, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        return immediateBytesTable[_opcode];
+    }
+    int modRMLength()
+    {
+        static const Byte modRMLengthsTable[] = {
+            0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 
+            0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 
+            0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 
+            0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 2, 0, 
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+            2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+            2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+            2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        if (hasModrm())
+            return 1 + modRMLengthsTable[_modrm];
+        return 0;
+    }
+    int length()
+    {
+        return 1 + modRMLength() + immediateBytes();
+    }
+    int defaultSegment()
+    {
+        if (hasModrm() && _opcode != 0x8d) {
+            static const signed char defaultSegmentTable[] = {
+                3, 3, 2, 2, 3, 3, 3, 3, 3, 3, 2, 2, 3, 3, 3, 3, 
+                3, 3, 2, 2, 3, 3, 3, 3, 3, 3, 2, 2, 3, 3, 3, 3, 
+                3, 3, 2, 2, 3, 3, 3, 3, 3, 3, 2, 2, 3, 3, 3, 3, 
+                3, 3, 2, 2, 3, 3, 3, 3, 3, 3, 2, 2, 3, 3, 3, 3, 
+                3, 3, 2, 2, 3, 3, 2, 3, 3, 3, 2, 2, 3, 3, 2, 3, 
+                3, 3, 2, 2, 3, 3, 2, 3, 3, 3, 2, 2, 3, 3, 2, 3, 
+                3, 3, 2, 2, 3, 3, 2, 3, 3, 3, 2, 2, 3, 3, 2, 3, 
+                3, 3, 2, 2, 3, 3, 2, 3, 3, 3, 2, 2, 3, 3, 2, 3, 
+                3, 3, 2, 2, 3, 3, 2, 3, 3, 3, 2, 2, 3, 3, 2, 3, 
+                3, 3, 2, 2, 3, 3, 2, 3, 3, 3, 2, 2, 3, 3, 2, 3, 
+                3, 3, 2, 2, 3, 3, 2, 3, 3, 3, 2, 2, 3, 3, 2, 3, 
+                3, 3, 2, 2, 3, 3, 2, 3, 3, 3, 2, 2, 3, 3, 2, 3, 
+               -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+               -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+               -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,
+               -1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1};
+            return defaultSegmentTable[_modrm];
+        }
+        int o = _opcode & 0xfe;
+        if (o == 0xa0 || o == 0xa2 || o == 0xa4 || o == 0xa6 || o == 0xac ||
+            _opcode == 0xd7)
+            return 3;
+        return -1;
+
+    }
+    void output(Byte* p)
+    {
+        *p = _opcode;
+        ++p;
+        if (hasModrm()) {
+            *p = _modrm;
+            ++p;
+            int l = modRMLength();
+            if (l > 0) {
+                *p = (_offset & 0xff);
+                if (l > 1)
+                    p[1] = _offset >> 8;
+                p += l;
+            }
+        }
+        int l = immediateBytes();
+        if (l > 0) {
+            *p = _immediate;
+            if (l > 1) {
+                p[1] = _immediate >> 8;
+                if (l == 4) {
+                    p[2] = _immediate >> 16;
+                    p[3] = _immediate >> 24;
+                }
+            }
+        }
+    }
+    bool isGroup()
+    {
+        int o = _opcode & 0xfe;
+        return o == 0xf6 || o == 0xfe || o == 0x80 || o == 0x82;
+    }
+    
+private:
+    Byte _opcode;
+    Byte _modrm;
+    Word _offset;
+    DWord _immediate;
+};
+
+class Test
+{
+public:
+    void addInstruction(Instruction instruction)
+    {
+        _instructions.append(instruction);
+    }
+    int length()
+    {
+        int l = 0;
+        for (auto i : _instructions)
+            l += i.length();
+        return l;
+    }
+    void output(Byte* p)
+    {
+        for (auto i : _instructions) {
+            int l = i.length();
+            i.output(p);
+            p += l;
+        }
+    }
+private:
+    AppendableArray<Instruction> _instructions;
+};
+
 class Program : public ProgramBase
 {
 public:
+    void run()
+    {
+        for (int i = 0; i < 0x100; ++i) {
+            Instruction instruction(i);
+            if (instruction.hasModrm()) {
+                static const Byte modrms[] = {
+                    0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+                    0x40, 0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47,
+                    0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0xc0};
+                int jj = 0;
+                if (instruction.isGroup())
+                    jj = 8;
+                for (int m = 0; m < 25; ++m) {
+                    for (int j = 0; j < jj; ++j)
+                        addTest(Instruction(i, modrms[m] + (j << 3)));
+                }
+            }
+            else
+                addTest(instruction);
+        }
 
+        printf("%i\n", _tests.count());
+    }
+private:
+    void addTest(Instruction i)
+    {
+        Test t;
+        t.addInstruction(i);
+        _tests.append(t);
+    }
+    
+    AppendableArray<Test> _tests;
 };
