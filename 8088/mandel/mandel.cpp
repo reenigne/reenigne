6decf465450dcb185cf49ae70c4d6a4c80f6e486
Andrew Jenner <andrew@reenigne.org>
2018-05-15 22:30:23 +0100

    Mandel subdivision

--- 8088/mandel/mandel.cpp
+++ 8088/mandel/mandel.cpp
@@ -60,7 +60,7 @@ public:
         _vram[CGAData::registerHorizontalSyncWidth] = 10; // 16;
         _vram[CGAData::registerVerticalTotal] = 128 - 1;
         _vram[CGAData::registerVerticalTotalAdjust] = 6;
-        _vram[CGAData::registerVerticalDisplayed] = 100;
+        _vram[CGAData::registerVerticalDisplayed] = 101;
         _vram[CGAData::registerVerticalSyncPosition] = 112;
         _vram[CGAData::registerInterlaceMode] = 2;
         _vram[CGAData::registerMaximumScanline] = 1;
@@ -81,71 +81,45 @@ public:
         _animated.setDrawWindow(this);
         _animated.setRate(60);
 
-        //int fracBits = 10; //11;
-        int frac = 0x600;
-
-        Word squares[0x8000];
+        _frac = 0x600;
+        _squares.allocate(0x8000);
         for (int i = 0; i < 0x8000; ++i) {
             int ii = i * 2;
-            int s;
             if (ii >= 0x8000)
                 ii -= 0x10000;
-            //if (ii >= 0x1000 || ii <= -0x1000)
-            //    s = 0x2000;
-            //else
-                //s = ((ii*ii /*+ (1 << (fracBits - 1))*/) >> fracBits) & 0xfffe;
-            s = ((ii*ii + (frac / 2)) / frac) & 0xfffe;
-            squares[i] = s;
+            _squares[i] = ((ii*ii + (_frac / 2)) / _frac) & 0xfffe;
         }
         //Byte colourTable[] = {0x00,
         //    0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff,
         //    0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff,
         //    0x11, 0x22};
         //Byte colourTable[] = {0x00,
-        //    0xff, 0xee, 0xaa, 0xbb, 0x99, 0x88, 0x11, 0x33, 0x22, 0x66, 0x77, 0x55, 0x44, 0xcc, 0xdd, 
-        //    0xff, 0xee, 0xaa, 0xbb, 0x99, 0x88, 0x11, 0x33, 0x22, 0x66, 0x77, 0x55, 0x44, 0xcc, 0xdd, 
+        //    0xff, 0xee, 0xaa, 0xbb, 0x99, 0x88, 0x11, 0x33, 0x22, 0x66, 0x77, 0x55, 0x44, 0xcc, 0xdd,
+        //    0xff, 0xee, 0xaa, 0xbb, 0x99, 0x88, 0x11, 0x33, 0x22, 0x66, 0x77, 0x55, 0x44, 0xcc, 0xdd,
         //    0xff, 0xee};
-        Byte colourTable[] = {0x00,
-            0xff, 0xff, 0xff, 0xff, 0xee, 0xee, 0xee, 0xee, 0xaa, 0xaa, 0xaa, 0xbb, 0xbb, 0xbb, 0x99, 
-            0x99, 0x99, 0x88, 0x88, 0x11, 0x11, 0x33, 0x33, 0x22, 0x22, 0x66, 0x77, 0x55, 0x44, 0xcc, 
-            0xdd, 0xff};
 
-        int mode = 1;
-        int modeIncrements[] = {1, 2, 4};
-        int modeMasks[] = {0x80, 0xc0, 0xf0};
+        _mode = 1;
+        static int modeIncrements[] = {1, 2, 4};
 
-        for (int yp = 0; yp < 201; ++yp) {
-            //int b = (((yp - 100) << fracBits)*3/200) & -2;
-            //int b = (((yp - 100) << fracBits)*9/4/200) & -2;
-            int b = (((yp - 100) * frac)*9/4/200) & -2;
-            for (int xp = 0; xp < 640; xp += modeIncrements[mode]) {
-                //int a = (((xp - 200) << fracBits)*4/320) & -2;
-                //int a = (((xp - 240) << fracBits)*3/320) & -2;
-                int a = (((xp -  480) * frac)*3/640) & -2;
-                int i;
-                int x = a;
-                int y = b;
-                for (i = 32; i > 0; --i) {
-                    int xx = squares[(x >> 1) & 0x7fff];
-                    int yy = squares[(y >> 1) & 0x7fff];
-                    int zz = xx + yy;
-                    //if (zz & 0x10000)
-                    //    break;
-                    zz &= 0xffff;
-                    //if ((xx & 0x8000) | (yy & 0x8000) | (zz & 0x8000))
-                    //    break;
-                    if (zz > 0x1c00 /*0x1800*/ /*|| xx >= 0x2000 || yy >= 0x2000*/)
-                        break;
-                    int xyxy = squares[((x + y) >> 1) & 0x7fff];
-                    int xy2 = xyxy - zz;
-                    y = xy2 + b;
-                    x = a + xx - yy;
-                }
-                int p = ((yp & 1) << 13) + (yp >> 1)*80 + (xp >> 3);
-                Byte m = modeMasks[mode] >> (xp & 7);
-                _vram[p] = (_vram[p] & ~m) + (colourTable[i] & m);
-            }
-        }
+        _iters.allocate(385*129);
+        for (int i = 0; i < 385*129; ++i)
+            _iters[i] = 0xff;
+
+        // Coarse grid initially
+        for (int yp = 0; yp < 256; yp += 128)
+            for (int xp = 0; xp < 1024; xp += 256)
+                mandelIters(xp, yp);
+        // Progressively refine
+        for (int xp = 0; xp < 768; xp += 256)
+            subdivide(xp, 0, 7);
+
+        //for (int yp = 0; yp < 201; ++yp) {
+        //    int b = bFromYp(yp);
+        //    for (int xp = 0; xp < 640; xp += modeIncrements[_mode]) {
+        //        int i = mandelIters(aFromXp(xp), b);
+        //        plot(xp, yp, i);
+        //    }
+        //}
     }
     ~MandelWindow() { join(); }
     void join() { _output->join(); }
@@ -168,6 +142,91 @@ public:
     CGAData* getData() { return &_data; }
     CGASequencer* getSequencer() { return &_sequencer; }
 private:
+    void subdivide(int xp, int yp, int s)
+    {
+        int z = 1 << s;
+        int a = _iters[yp*385 + (xp >> 1)];
+        int b = _iters[yp*385 + (xp >> 1) + z];
+        int c = _iters[(yp + z)*385 + (xp >> 1)];
+        int d = _iters[(yp + z)*385 + (xp >> 1) + z];
+        if (a == b && a == c || a == d) {
+            int w = 1 << s;
+            for (int y = 0; y < w; ++y)
+                for (int x = 0; x < w; ++x)
+                    plot(xp + (x << 1), yp + y, a);
+            return;
+        }
+        int h = 1 << (s - 1);
+        int u = (a == b ? a : mandelIters(xp + z, yp));
+        _iters[yp*385 + (xp >> 1) + h] = u;
+        int v = (a == c ? a : mandelIters(xp, yp + h));
+        _iters[(yp + h)*385 + (xp >> 1)] = v;
+        int p = (b == d ? d : mandelIters(xp + z + z, yp + h));
+        _iters[(yp + h)*385 + (xp >> 1) + z] = p;
+        int q = (c == d ? d : mandelIters(xp + z, yp + z));
+        _iters[(yp + z)*385 + (xp >> 1) + h] = q;
+        int w = (u == v && u == p && u == q ? u : mandelIters(xp + z, yp + h));
+        _iters[(yp + h)*385 + (xp >> 1) + h] = w;
+        if (s > 1) {
+            subdivide(xp, yp, s - 1);
+            subdivide(xp + z, yp, s - 1);
+            subdivide(xp, yp + h, s - 1);
+            subdivide(xp + z, yp + h, s - 1);
+        }
+    }
+    void plot(int xp, int yp, int i)
+    {
+        _iters[yp*385 + (xp >> 1)] = i;
+        if (xp >= 320 || yp > 100)
+            return;
+        plot2(xp, yp+100, i);
+        plot2(xp, 100-yp, i);
+    }
+    void plot2(int xp, int yp, int i)
+    {
+        static Byte colourTable[] = {
+            0x00, 0xff, 0xff, 0xff, 0xff, 0xee, 0xee, 0xee, 0xee, 0xaa, 0xaa,
+            0xaa, 0xbb, 0xbb, 0xbb, 0x99, 0x99, 0x99, 0x88, 0x88, 0x11, 0x11,
+            0x33, 0x33, 0x22, 0x22, 0x66, 0x77, 0x55, 0x44, 0xcc, 0xdd, 0xff};
+        static int modeMasks[] = {0x80, 0xc0, 0xf0};
+        int p = ((yp & 1) << 13) + (yp >> 1)*80 + (xp >> 3);
+        Byte m = modeMasks[_mode] >> (xp & 7);
+        _vram[p] = (_vram[p] & ~m) + (colourTable[i] & m);
+    }
+    int aFromXp(int xp)
+    {
+        return (((xp - 240) * _frac)*3/320) & -2;
+    }
+    int bFromYp(int yp)
+    {
+        return (((yp - 100) * _frac)*9/4/200) & -2;
+    }
+    int mandelIters(int xp, int yp)
+    {
+        if (_iters[yp*385 + (xp >> 1)] != 0xff)
+            return _iters[yp*385 + (xp >> 1)];
+        int a = aFromXp(xp);
+        int b = bFromYp(yp);
+        int x = a;
+        int y = b;
+        int i;
+        for (i = 32; i > 0; --i) {
+            int xx = _squares[(x >> 1) & 0x7fff];
+            int yy = _squares[(y >> 1) & 0x7fff];
+            int zz = xx + yy;
+            zz &= 0xffff;
+            if (zz > 0x1c00)
+                break;
+            y = (_squares[((x + y) >> 1) & 0x7fff] - zz) + b;
+            x = a + xx - yy;
+        }
+        plot(xp, yp, i);
+        return i;
+    }
+    int _mode;
+    Array<Byte> _iters;
+    Array<Word> _squares;
+    int _frac;
     CGAData _data;
     CGASequencer _sequencer;
     CGAOutput* _output;
