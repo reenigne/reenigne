3ac23ac6039cefae58741b7038ebf4aedbb6aa4b
Andrew Jenner <andrew@reenigne.org>
2020-04-06 09:59:04 +0100

    Fix typo in wrap.h

--- include/alfe/wrap.h
+++ include/alfe/wrap.h
@@ -3,7 +3,7 @@
 #ifndef INCLUDED_WRAP_H
 #define INCLUDED_WRAP_H
 
-// Works like the % operator but doesn't refect around 0
+// Works like the % operator but doesn't reflect around 0
 template<class T> T wrap(const T& a, const T& b)
 {
     return a < 0 ? b - (-a)%b : a%b;
