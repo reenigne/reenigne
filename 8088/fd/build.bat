f2d4db0694ca273203d7685b90648477dfa1ad36
Andrew Jenner <andrew@reenigne.org>
2020-07-11 17:23:27 +0100

    Write and boot real FD image with verification

--- 8088/fd/build.bat
+++ 8088/fd/build.bat
@@ -2,6 +2,7 @@
 yasm fd.asm -o fd.bin
 yasm write_image.asm -o write_image.bin
 yasm write_image2.asm -o write_image2.bin
+yasm write_verify_image.asm -o write_verify_image.bin
 yasm read_image.asm -o read_image.bin
 yasm 765.asm -o 765.bin
 yasm fdspeed.asm -o fd.com
---
+++ 8088/fd/verify_boot_image.bat
@@ -0,0 +1,2 @@
+copy /b write_verify_image.bin+%1 f.bin
+xtrun f.bin
